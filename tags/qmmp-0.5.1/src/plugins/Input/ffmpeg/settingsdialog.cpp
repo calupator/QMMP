853f39611bd13be11b3a152fe51430c0dcc95634
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2011-05-20 15:18:35 +0000

    tagging 0.5.1 release

---
+++ tags/qmmp-0.5.1/src/plugins/Input/ffmpeg/settingsdialog.cpp
@@ -0,0 +1,93 @@
+/***************************************************************************
+ *   Copyright (C) 2008-2011 by Ilya Kotov                                 *
+ *   forkotov02@hotmail.ru                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QSettings>
+#include <QStringList>
+
+#include <qmmp/qmmp.h>
+
+extern "C"{
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+}
+#include "settingsdialog.h"
+
+SettingsDialog::SettingsDialog(QWidget *parent)
+        : QDialog(parent)
+{
+    ui.setupUi(this);
+    setAttribute(Qt::WA_DeleteOnClose);
+    QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
+    QStringList filters;
+    filters << "*.wma";
+    filters << "*.ape";
+    filters = settings.value("FFMPEG/filters", filters).toStringList();
+    avcodec_init();
+    avcodec_register_all();
+    av_register_all();
+    ui.wmaCheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_WMAV1));
+    ui.wmaCheckBox->setChecked(filters.contains("*.wma") && avcodec_find_decoder(CODEC_ID_WMAV1));
+    ui.apeCheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_APE));
+    ui.apeCheckBox->setChecked(filters.contains("*.ape") && avcodec_find_decoder(CODEC_ID_APE));
+    ui.ttaCheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_TTA));
+    ui.ttaCheckBox->setChecked(filters.contains("*.tta") && avcodec_find_decoder(CODEC_ID_TTA));
+    ui.aacCheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_AAC));
+    ui.aacCheckBox->setChecked(filters.contains("*.aac") && avcodec_find_decoder(CODEC_ID_AAC));
+    ui.mp3CheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_MP3));
+    ui.mp3CheckBox->setChecked(filters.contains("*.mp3") && avcodec_find_decoder(CODEC_ID_MP3));
+    ui.mp4CheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_AAC));
+    ui.mp4CheckBox->setChecked(filters.contains("*.m4a") && (avcodec_find_decoder(CODEC_ID_AAC)
+                                                             || avcodec_find_decoder(CODEC_ID_ALAC)));
+    ui.raCheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_RA_288));
+    ui.raCheckBox->setChecked(filters.contains("*.ra") && avcodec_find_decoder(CODEC_ID_RA_288));
+    ui.shCheckBox->setChecked(filters.contains("*.shn") && avcodec_find_decoder(CODEC_ID_SHORTEN));
+    ui.ac3CheckBox->setEnabled(avcodec_find_decoder(CODEC_ID_AC3));
+    ui.ac3CheckBox->setChecked(filters.contains("*.ac3") && avcodec_find_decoder(CODEC_ID_AC3));
+}
+
+SettingsDialog::~SettingsDialog()
+{
+}
+
+void SettingsDialog::accept()
+{
+    QStringList filters;
+    if (ui.mp3CheckBox->isChecked())
+        filters << "*.mp3";
+    if (ui.wmaCheckBox->isChecked())
+        filters << "*.wma";
+    if (ui.apeCheckBox->isChecked())
+        filters << "*.ape";
+    if (ui.ttaCheckBox->isChecked())
+        filters << "*.tta";
+    if (ui.aacCheckBox->isChecked())
+        filters << "*.aac";
+    if (ui.mp4CheckBox->isChecked())
+        filters << "*.m4a";
+    if (ui.raCheckBox->isChecked())
+        filters << "*.ra";
+    if (ui.shCheckBox->isChecked())
+        filters << "*.shn";
+    if (ui.ac3CheckBox->isChecked())
+        filters << "*.ac3";
+    QSettings settings(Qmmp::configFile(), QSettings::IniFormat);
+    settings.setValue("FFMPEG/filters", filters);
+    QDialog::accept();
+}
---
+++ tags/qmmp-0.5.1/src/plugins/Input/ffmpeg/translations/ffmpeg_plugin_de.ts
@@ -0,0 +1,139 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>DecoderFFmpegFactory</name>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="187"/>
+        <source>About FFmpeg Audio Plugin</source>
+        <translation>Über FFmpeg-Audio-Modul</translation>
+    </message>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="189"/>
+        <source>Qmmp FFmpeg Audio Plugin</source>
+        <translation>Qmmp FFmpeg-Audio-Modul</translation>
+    </message>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="199"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autor: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="84"/>
+        <source>FFmpeg Plugin</source>
+        <translation>FFmpeg-Modul</translation>
+    </message>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="86"/>
+        <source>FFmpeg Formats</source>
+        <translation>FFmpeg-Formate</translation>
+    </message>
+    <message>
+        <location filename="../decoderffmpegfactory.cpp" line="191"/>
+        <source>Compiled against libavformat-%1.%2.%3 and libavcodec-%4.%5.%6</source>
+        <translation>Kompiliert gegen libavformat-%1.%2.%3 und libavcodec-%4.%5.%6</translation>
+    </message>
+</context>
+<context>
+    <name>FFmpegMetaDataModel</name>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="48"/>
+        <source>Length</source>
+        <translation>Länge</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="49"/>
+        <source>File size</source>
+        <translation>Dateigröße</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="49"/>
+        <source>KB</source>
+        <translation>KB</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="50"/>
+        <source>Bitrate</source>
+        <translation>Bitrate</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="50"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="66"/>
+        <source>Sample rate</source>
+        <translation>Abtastrate</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="66"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../ffmpegmetadatamodel.cpp" line="67"/>
+        <source>Channels</source>
+        <translation>Kanäle</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="55"/>
+        <source>Formats</source>
+        <translation>Formate</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="61"/>
+        <source>Windows Media Audio</source>
+        <translation>Windows Media Audio</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="68"/>
+        <source>Monkey&apos;s Audio</source>
+        <translation>Monkey&apos;s Audio</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="75"/>
+        <source>True Audio</source>
+        <translation>True Audio</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="82"/>
+        <source>ADTS AAC</source>
+        <translation>ADTS AAC</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="96"/>
+        <source>MPEG-4 AAC/ALAC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="103"/>
+        <source>RealAudio 1.0/2.0</source>
+        <translation>RealAudio 1.0/2.0</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="110"/>
+        <source>Shorten</source>
+        <translatorcomment>Name eines Dateiformats</translatorcomment>
+        <translation>Shorten</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="117"/>
+        <source>AC-3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>FFmpeg Plugin Settings</source>
+        <translation>Einstellungen FFmpeg-Modul</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>MP3 (MPEG audio layer 3)</source>
+        <translation>MP3 (MPEG Audio Layer 3)</translation>
+    </message>
+</context>
+</TS>
