19bb099be7c9659a08b4839a3ead76e75636bfe0
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2012-07-29 17:52:28 +0000

    tagging 0.6.2 release

---
+++ tags/qmmp-0.6.2/src/qmmp/volume.h
@@ -0,0 +1,53 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Ilya Kotov                                      *
+ *   forkotov02@hotmail.ru                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef VOLUME_H
+#define VOLUME_H
+
+#endif // VOLUME_H
+
+
+/*! @brief The Volume class is a provides volume API
+ * @author Ilya Kotov <forkotov02@hotmail.ru>
+ */
+class Volume
+{
+public:
+    virtual ~Volume(){}
+    /*!
+     * Volume control channel enum
+     */
+    enum Channel
+    {
+        LEFT_CHANNEL = 0, /*!< Right channel */
+        RIGHT_CHANNEL     /*!< Left channel */
+    };
+    /*!
+     * Setups volume level.
+     * Subclass should reimplement this fucntion.
+     * @param channel Channel
+     * @param value Volume level. It should be \b 0..100
+     */
+    virtual void setVolume(int channel, int value) = 0;
+    /*!
+     * Returns volume level of the \b channel.
+     */
+    virtual int volume(int channel) = 0;
+};
