61046e19139cf0da5ad6806b4128e75c524ea85b
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-01-11 07:12:03 +0000

    tagging plugin pack 0.6.4

---
+++ tags/qmmp-plugin-pack-0.6.4/src/Ui/qsui/positionslider.cpp
@@ -0,0 +1,44 @@
+/***************************************************************************
+ *   Copyright (C) 2011-2012 by Ilya Kotov                                 *
+ *   forkotov02@hotmail.ru                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include "positionslider.h"
+
+PositionSlider::PositionSlider(QWidget *parent) : QSlider(Qt::Horizontal, parent)
+{
+}
+
+void PositionSlider::mousePressEvent (QMouseEvent *event)
+{
+	if (event->button() == Qt::LeftButton)
+	{
+		if (orientation() == Qt::Vertical)
+			setValue(minimum() + ((maximum()-minimum()) * (height()-event->y())) / height() ) ;
+		else
+			setValue(minimum() + ((maximum()-minimum()) * event->x()) / width());
+		setSliderDown (true);
+		event->accept();
+	}
+	QSlider::mousePressEvent(event);
+}
+
+void PositionSlider::mouseReleaseEvent (QMouseEvent *)
+{
+	setSliderDown (false);
+}
