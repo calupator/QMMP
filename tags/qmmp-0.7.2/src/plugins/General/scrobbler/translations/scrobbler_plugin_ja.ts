1539ea2b3669581e75708368c2628442e29f8229
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-08-26 17:58:10 +0000

    tagging 0.7.2 release

---
+++ tags/qmmp-0.7.2/src/plugins/General/scrobbler/translations/scrobbler_plugin_ja.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>ScrobblerFactory</name>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="30"/>
+        <source>Scrobbler Plugin</source>
+        <translation>Scrobbler プラグイン</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="50"/>
+        <source>About Scrobbler Plugin</source>
+        <translation>Scrobbler プラグインについて</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="51"/>
+        <source>Qmmp AudioScrobbler Plugin</source>
+        <translation>QMMP Scrobbler プラグイン</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>制作: Илья Котов (Ilya Kotov) &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Scrobbler Plugin Settings</source>
+        <translation>Scrobbler プラグイン設定</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="32"/>
+        <source>Last.fm</source>
+        <translation>Lastfm.jp</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="43"/>
+        <source>Session:</source>
+        <translation>セッション:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="53"/>
+        <source>Check</source>
+        <translation>試動</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="75"/>
+        <source>Register new session</source>
+        <translation>新しいセッションを登録</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="94"/>
+        <source>User name:</source>
+        <translation>ユーザー名:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="104"/>
+        <source>Password:</source>
+        <translation>パスワード:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="85"/>
+        <source>Libre.fm</source>
+        <translation>Libre.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="73"/>
+        <location filename="../settingsdialog.cpp" line="96"/>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>Message</source>
+        <translation>メッセージ</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="74"/>
+        <source>1. Wait for browser startup</source>
+        <translation>1. ブラウザーが開きます。</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="75"/>
+        <source>2. Allow Qmmp to scrobble tracks to your Last.fm account</source>
+        <translation>2. 設定ページの案内に従い、 QMMP が演目を Last.fm アカウントに scrobble 送信できるようにします。</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="76"/>
+        <source>3. Press &quot;OK&quot;</source>
+        <translation>3. &quot;OK&quot; ボタンをクリックします。</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="81"/>
+        <location filename="../settingsdialog.cpp" line="104"/>
+        <location filename="../settingsdialog.cpp" line="133"/>
+        <source>Network error</source>
+        <translation>ネットワークエラー</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="85"/>
+        <location filename="../settingsdialog.cpp" line="108"/>
+        <source>Unable to register new session</source>
+        <translation>新規セッションを登録できません</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="96"/>
+        <source>New session has been received successfully</source>
+        <translation>新規セッションは受理されました</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>Permission granted</source>
+        <translation>許可</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="137"/>
+        <source>Permission denied</source>
+        <translation>不許可</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="81"/>
+        <location filename="../settingsdialog.cpp" line="85"/>
+        <location filename="../settingsdialog.cpp" line="104"/>
+        <location filename="../settingsdialog.cpp" line="108"/>
+        <location filename="../settingsdialog.cpp" line="133"/>
+        <location filename="../settingsdialog.cpp" line="137"/>
+        <source>Error</source>
+        <translation>事故発生</translation>
+    </message>
+</context>
+</TS>
