a144cf9283f0e160279919bfb9ebf0fb8220d224
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2013-12-24 06:25:04 +0000

    tagging qmmp-0.7.4 release

---
+++ src/plugins/Visual/analyzer/translations/analyzer_plugin_it.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it">
+<context>
+    <name>Analyzer</name>
+    <message>
+        <location filename="../analyzer.cpp" line="56"/>
+        <source>Qmmp Analyzer</source>
+        <translation>Analizzatore Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="77"/>
+        <source>F</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColorWidget</name>
+    <message>
+        <location filename="../colorwidget.cpp" line="37"/>
+        <source>Select Color</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Analyzer Plugin Settings</source>
+        <translation>Impostazioni Del modulo Analizzatore</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="26"/>
+        <source>General</source>
+        <translation>Generale</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Show peaks</source>
+        <translation>Mostra i picchi</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="42"/>
+        <source>Analyzer falloff:</source>
+        <translation>Ricaduta analizzatore</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="56"/>
+        <location filename="../settingsdialog.ui" line="98"/>
+        <source>Slowest</source>
+        <translation>Molto lenta</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="61"/>
+        <location filename="../settingsdialog.ui" line="103"/>
+        <source>Slow</source>
+        <translation>Lenta</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="66"/>
+        <location filename="../settingsdialog.ui" line="108"/>
+        <source>Medium</source>
+        <translation>Media</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="71"/>
+        <location filename="../settingsdialog.ui" line="113"/>
+        <source>Fast</source>
+        <translation>Rapida</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="76"/>
+        <location filename="../settingsdialog.ui" line="118"/>
+        <source>Fastest</source>
+        <translation>Molto rapida</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="84"/>
+        <source>Peaks falloff:</source>
+        <translation>Ricaduta picchi</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="126"/>
+        <source>Refresh rate:</source>
+        <translation>Velocità di aggiornamento:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="140"/>
+        <source>50 FPS</source>
+        <translation>50 FPS</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="145"/>
+        <source>25 FPS</source>
+        <translation>25 FPS</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="150"/>
+        <source>10 FPS</source>
+        <translation>10 FPS</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="155"/>
+        <source>5 FPS</source>
+        <translation>5 FPS</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <source>Cells size:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="223"/>
+        <source>Colors</source>
+        <translation>Colori</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="229"/>
+        <source>Peaks:</source>
+        <translation>Picchi:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="261"/>
+        <source>Analyzer #1:</source>
+        <translation>Analizzatore Nr. 1:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="293"/>
+        <source>Background:</source>
+        <translation>Sfondo:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="325"/>
+        <source>Analyzer #2:</source>
+        <translation>Analizzatore N° 2:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="370"/>
+        <source>Analyzer #3:</source>
+        <translation>Analizzatore N° 3:</translation>
+    </message>
+</context>
+<context>
+    <name>VisualAnalyzerFactory</name>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="30"/>
+        <source>Analyzer Plugin</source>
+        <translation>Modulo analizzatore</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="49"/>
+        <source>About Analyzer Visual Plugin</source>
+        <translation>Info sul modulo Analizzatore visivo</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="50"/>
+        <source>Qmmp Analyzer Visual Plugin</source>
+        <translation>Modulo Analizzatore visivo Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="51"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autore: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+</TS>
