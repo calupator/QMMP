457bce7dd6abfba06a1f59cf81b26cef0b83412d
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-02-11 15:15:59 +0000

    tagging qmmp-0.7.5

---
+++ tags/qmmp-0.7.5/src/app/translations/qmmp_uk_UA.ts
@@ -0,0 +1,125 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="uk">
+<context>
+    <name>BuiltinCommandLineOption</name>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="70"/>
+        <source>Skip forward in playlist</source>
+        <translation>Перейти до наступного фрагменту</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="71"/>
+        <source>Skip backwards in playlist</source>
+        <translation>Перейти до попереднього фрагменту</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="63"/>
+        <source>Start playing current song</source>
+        <translation>Грати поточну пісню</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="62"/>
+        <source>Don&apos;t clear the playlist</source>
+        <translation>Не очищати список</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="64"/>
+        <source>Pause current song</source>
+        <translation>Призупитини поточну пісню</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="65"/>
+        <source>Pause if playing, play otherwise</source>
+        <translation>Призупинити/відтворити</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="66"/>
+        <source>Stop current song</source>
+        <translation>Зупитини поточну пісню</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="67"/>
+        <source>Display Jump to File dialog</source>
+        <translation>Показати діалог переходу до файла</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="68"/>
+        <source>Quit application</source>
+        <translation>Закрити програму</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="69"/>
+        <source>Set playback volume (example: qmmp --volume 20)</source>
+        <translation>Встановити гучність (приклад: qmmp --volume 20)</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="72"/>
+        <source>Show/hide application</source>
+        <translation>Показати/сховати програму</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="73"/>
+        <source>Display Add File dialog</source>
+        <translation>Показати діалог додавання файлів</translation>
+    </message>
+    <message>
+        <location filename="../builtincommandlineoption.cpp" line="74"/>
+        <source>Display Add Directory dialog</source>
+        <translation>Показати діалог додавання тек</translation>
+    </message>
+</context>
+<context>
+    <name>QMMPStarter</name>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="286"/>
+        <source>Usage: qmmp [options] [files]</source>
+        <translation>Використання: qmmp [options] [files]</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="287"/>
+        <source>Options:</source>
+        <translation>Опції:</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="291"/>
+        <source>Don&apos;t start the application</source>
+        <translation>Не запускати програму</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="293"/>
+        <source>Print version number and exit</source>
+        <translation>Показати версію та вийти</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="294"/>
+        <source>Ideas, patches, bugreports send to forkotov02@hotmail.ru</source>
+        <translation>Ідеї, виправлення, звіти про помилки:  forkotov02@hotmail.ru</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="299"/>
+        <source>QMMP version: %1</source>
+        <translation>Версія QMMP: %1</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="300"/>
+        <source>Compiled with Qt version: %1</source>
+        <translation>Зібрано з Qt версії: %1</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="301"/>
+        <source>Using Qt version: %1</source>
+        <translation>Використовується Qt версії: %1</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="292"/>
+        <source>Display this text and exit</source>
+        <translation>Показати цей текст та вийти</translation>
+    </message>
+    <message>
+        <location filename="../qmmpstarter.cpp" line="83"/>
+        <source>Unknown command</source>
+        <translation>Невідома команда</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-0.7.5/src/plugins/General/hotkey/hotkeymanager_win.cpp
@@ -0,0 +1,285 @@
+/***************************************************************************
+ *   Copyright (C) 2013 by Ilya Kotov                                      *
+ *   forkotov02@hotmail.ru                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#include <QtGlobal>
+#ifdef Q_OS_WIN
+#include <QSettings>
+#include <QX11Info>
+#include <QEvent>
+#include <QKeyEvent>
+#include <QCoreApplication>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <windows.h>
+#include <winuser.h>
+#include <commctrl.h>
+#include <qmmp/qmmp.h>
+#include <qmmp/soundcore.h>
+#include <qmmpui/mediaplayer.h>
+#include <qmmpui/uihelper.h>
+#include "hotkeymanager.h"
+
+static struct
+{
+    unsigned int key; //virtual key
+    unsigned long code; //scan code
+} keyMap [] = {
+    { VK_LEFT,    0x14b },
+    { VK_UP,      0x148 },
+    { VK_RIGHT,   0x14d },
+    { VK_DOWN,    0x150 },
+    { VK_PRIOR,   0x149 },
+    { VK_NEXT,    0x151 },
+    { VK_END,     0x14F },
+    { VK_HOME,    0x147 },
+    { VK_INSERT,  0x152 },
+    { VK_DELETE,  0x153 },
+    { VK_DIVIDE,  0x135 },
+    { VK_NUMLOCK, 0x145 },
+{ 0, 0 }
+};
+
+quint32 Hotkey::defaultKey()
+{
+    return defaultKey(action);
+}
+
+quint32 Hotkey::defaultKey(int act)
+{
+    //default key bindings
+    QMap<int, quint32> keyMap;
+    keyMap[PLAY] = 0;
+    keyMap[STOP] = 0;
+    keyMap[PAUSE] = 0;
+    keyMap[PLAY_PAUSE] = 0;
+    keyMap[NEXT] = 0;
+    keyMap[PREVIOUS] = 0;
+    keyMap[SHOW_HIDE] = 0;
+    keyMap[VOLUME_UP] = 0;
+    keyMap[VOLUME_DOWN] = 0;
+    keyMap[FORWARD] = 0;
+    keyMap[REWIND] = 0;
+    keyMap[JUMP_TO_TRACK] = 0;
+    return keyMap[act];
+}
+
+class KeyFilterWidget : public QWidget
+{
+public:
+    KeyFilterWidget(const Hotkey &hotkey)
+    {
+        m_hotkey = hotkey;
+        m_mods = 0;
+        m_id = 0;
+
+        if(m_hotkey.mod & HOTKEYF_CONTROL)
+            m_mods |= MOD_CONTROL;
+        if(m_hotkey.mod & HOTKEYF_SHIFT)
+            m_mods |= MOD_SHIFT;
+        if(m_hotkey.mod & HOTKEYF_ALT)
+            m_mods |= MOD_ALT;
+        if(m_hotkey.mod & HOTKEYF_EXT)
+            m_mods |= MOD_WIN;
+
+
+        if(RegisterHotKey(winId(), m_mods^m_hotkey.key,  m_mods, m_hotkey.key))
+        {
+            m_id = m_mods^m_hotkey.key;
+            qDebug("KeyFilterWidget: registered key=0x%x, mod=0x%x", hotkey.key, m_mods);
+        }
+        else
+            qWarning("KeyFilterWidget: unable to register key=0x%x, mod=0x%x", hotkey.key, m_mods);
+    }
+
+    ~KeyFilterWidget()
+    {
+        if(m_id)
+            UnregisterHotKey(winId(), m_id);
+    }
+
+    bool winEvent(MSG* m, long* result)
+    {
+        if (m->message == WM_HOTKEY && m->wParam == m_id)
+        {
+            SoundCore *core = SoundCore::instance();
+            MediaPlayer *player = MediaPlayer::instance();
+            qDebug("KeyFilterWidget: [%s] pressed",
+                   qPrintable(HotkeyManager::getKeyString(m_hotkey.key, m_hotkey.mod)));
+
+            switch (m_hotkey.action)
+            {
+            case Hotkey::PLAY:
+                player->play();
+                break;
+            case Hotkey::STOP:
+                player->stop();
+                break;
+            case Hotkey::PAUSE:
+                core->pause();
+                break;
+            case Hotkey::PLAY_PAUSE:
+                if (core->state() == Qmmp::Stopped)
+                    player->play();
+                else if (core->state() != Qmmp::FatalError)
+                    core->pause();
+                break;
+            case Hotkey::NEXT:
+                player->next();
+                break;
+            case Hotkey::PREVIOUS:
+                player->previous();
+                break;
+            case Hotkey::SHOW_HIDE:
+                UiHelper::instance()->toggleVisibility();
+                break;
+            case Hotkey::VOLUME_UP:
+            case Hotkey::VOLUME_DOWN:
+            {
+                int volume = qMax(core->leftVolume(), core->rightVolume());
+                int balance = 0;
+                int left = core->leftVolume();
+                int right = core->rightVolume();
+                if (left || right)
+                    balance = (right - left)*100/volume;
+                if(m_hotkey.action == Hotkey::VOLUME_UP)
+                    volume = qMin (100, volume + 5);
+                else
+                    volume = qMax (0, volume - 5);
+                core->setVolume(volume-qMax(balance,0)*volume/100,
+                                volume+qMin(balance,0)*volume/100);
+            }
+                break;
+            case Hotkey::FORWARD:
+                core->seek(core->elapsed() + 5000);
+                break;
+            case Hotkey::REWIND:
+                core->seek(qMax(qint64(0), core->elapsed() - 5000));
+                break;
+            case Hotkey::JUMP_TO_TRACK:
+                UiHelper::instance()->jumpToTrack();
+                break;
+
+            }
+            qApp->processEvents();
+            return true;
+        }
+        return QWidget::winEvent(m, result);
+    }
+
+private:
+    Hotkey m_hotkey;
+    UINT m_mods;
+    WPARAM m_id;
+
+
+};
+
+HotkeyManager::HotkeyManager(QObject *parent) : QObject(parent)
+{
+    QCoreApplication::instance()->installEventFilter(this);
+
+
+    QSettings settings(Qmmp::configFile(), QSettings::IniFormat); //load settings
+    settings.beginGroup("Hotkey");
+    for (int i = Hotkey::PLAY; i <= Hotkey::JUMP_TO_TRACK; ++i)
+    {
+        quint32 key = settings.value(QString("key_%1").arg(i), Hotkey::defaultKey(i)).toUInt();
+        quint32 mod = settings.value(QString("modifiers_%1").arg(i), 0).toUInt();
+
+        if (key)
+        {
+            Hotkey hotkey;
+            hotkey.action = i;
+            hotkey.key = key;
+            hotkey.code = MapVirtualKey(key, 0);
+            hotkey.mod = mod;
+
+            KeyFilterWidget *filerWidget = new KeyFilterWidget(hotkey);
+            m_filters << filerWidget;
+        }
+    }
+    settings.endGroup();
+}
+
+HotkeyManager::~HotkeyManager()
+{
+    qDeleteAll(m_filters);
+}
+
+const QString HotkeyManager::getKeyString(quint32 key, quint32 modifiers)
+{
+    QString strModList[] = { "Ctrl", "Shift", "Alt", "Win"};
+    quint32 modList[] = { HOTKEYF_CONTROL, HOTKEYF_SHIFT, HOTKEYF_ALT, HOTKEYF_EXT};
+
+    QString keyStr;
+    for (int j = 0; j < 4; j++)
+    {
+        if (modifiers & modList[j])
+            keyStr.append(strModList[j] + "+");
+    }
+
+    if(key == VK_SHIFT || key == VK_CONTROL || key == VK_LWIN || key == VK_RWIN || key == VK_MENU)
+        return keyStr;
+
+    LONG lScan = MapVirtualKey(key, 0) << 16;
+
+    for(int i = 0; i < 12; ++i)
+    {
+        if(keyMap[i].key == key)
+        {
+            lScan |= 0x1000000; // set extended bit
+            break;
+        }
+    }
+
+    int nBufferLen = 64;
+    std::wstring str;
+    int nLen;
+    do
+    {
+        nBufferLen *= 2;
+        str.resize(nBufferLen);
+        nLen = ::GetKeyNameTextW(lScan, &str[0], nBufferLen);
+    }
+    while (nLen == nBufferLen);
+
+    keyStr += QString::fromWCharArray(str.c_str());
+
+    return keyStr;
+}
+
+QList<long> HotkeyManager::ignModifiersList()
+{
+    return QList<long>();
+}
+
+quint32 HotkeyManager::keycodeToKeysym(quint32 keycode)
+{
+    //MapVirtualKey does not work with some scan codes
+    //using hardcoded key map instead
+    for(int i = 0; i < 12; ++i)
+    {
+        if(keyMap[i].code == keycode)
+            return keyMap[i].key;
+    }
+    return MapVirtualKey(keycode, 1);
+}
+#endif
+
