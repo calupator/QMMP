a5f0fc734ae8896d7c8c529eda328c3f9c4abdce
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2014-04-05 08:19:59 +0000

    tagging 0.7.6 release

---
+++ src/plugins/General/scrobbler/translations/scrobbler_plugin_gl_ES.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="gl_ES">
+<context>
+    <name>ScrobblerFactory</name>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="30"/>
+        <source>Scrobbler Plugin</source>
+        <translation>Engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="50"/>
+        <source>About Scrobbler Plugin</source>
+        <translation>Sobre o engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="51"/>
+        <source>Qmmp AudioScrobbler Plugin</source>
+        <translation>Engadio AudioScrobbler de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Escrito por: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Scrobbler Plugin Settings</source>
+        <translation>Preferencias do engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="32"/>
+        <source>Last.fm</source>
+        <translation>Last.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="43"/>
+        <source>Session:</source>
+        <translation>Sesión:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="53"/>
+        <source>Check</source>
+        <translation>Comprobar</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="75"/>
+        <source>Register new session</source>
+        <translation>Rexistrar nova sesión</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="85"/>
+        <source>Libre.fm</source>
+        <translation>Libre.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="94"/>
+        <source>User name:</source>
+        <translation>Nome de usuario:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="104"/>
+        <source>Password:</source>
+        <translation>Contrasinal:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="73"/>
+        <location filename="../settingsdialog.cpp" line="96"/>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>Message</source>
+        <translation>Mensaxe</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="74"/>
+        <source>1. Wait for browser startup</source>
+        <translation>1. Esperar a que o navegador inicie</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="75"/>
+        <source>2. Allow Qmmp to scrobble tracks to your Last.fm account</source>
+        <translation>2. Permitir a Qmmp publicar as túas escoitas na conta Last.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="76"/>
+        <source>3. Press &quot;OK&quot;</source>
+        <translation>3. Prema &quot;OK&quot;</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="81"/>
+        <location filename="../settingsdialog.cpp" line="85"/>
+        <location filename="../settingsdialog.cpp" line="104"/>
+        <location filename="../settingsdialog.cpp" line="108"/>
+        <location filename="../settingsdialog.cpp" line="133"/>
+        <location filename="../settingsdialog.cpp" line="137"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="81"/>
+        <location filename="../settingsdialog.cpp" line="104"/>
+        <location filename="../settingsdialog.cpp" line="133"/>
+        <source>Network error</source>
+        <translation>Erro de rede</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="85"/>
+        <location filename="../settingsdialog.cpp" line="108"/>
+        <source>Unable to register new session</source>
+        <translation>Non se puido rexistrar nova sesión</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="96"/>
+        <source>New session has been received successfully</source>
+        <translation>A nova sesión recibiuse correctamente</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>Permission granted</source>
+        <translation>Permiso concedido</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="137"/>
+        <source>Permission denied</source>
+        <translation>Permiso denegado</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/Visual/analyzer/translations/analyzer_plugin_cs.ts
@@ -0,0 +1,166 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<context>
+    <name>Analyzer</name>
+    <message>
+        <location filename="../analyzer.cpp" line="56"/>
+        <source>Qmmp Analyzer</source>
+        <translation>Frekvenční analyzátor Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="77"/>
+        <source>F</source>
+        <translation>F</translation>
+    </message>
+</context>
+<context>
+    <name>ColorWidget</name>
+    <message>
+        <location filename="../colorwidget.cpp" line="37"/>
+        <source>Select Color</source>
+        <translation>Vyberte barvu</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Analyzer Plugin Settings</source>
+        <translation>Nastavení modulu frekvenčního analyzátoru</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="26"/>
+        <source>General</source>
+        <translation>Obecné</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Show peaks</source>
+        <translation>Zobrazovat špičky</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="42"/>
+        <source>Analyzer falloff:</source>
+        <translation>Pokles analyzátoru:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="56"/>
+        <location filename="../settingsdialog.ui" line="98"/>
+        <source>Slowest</source>
+        <translation>Nejpomalejší</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="61"/>
+        <location filename="../settingsdialog.ui" line="103"/>
+        <source>Slow</source>
+        <translation>Pomalý</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="66"/>
+        <location filename="../settingsdialog.ui" line="108"/>
+        <source>Medium</source>
+        <translation>Střední</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="71"/>
+        <location filename="../settingsdialog.ui" line="113"/>
+        <source>Fast</source>
+        <translation>Rychlý</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="76"/>
+        <location filename="../settingsdialog.ui" line="118"/>
+        <source>Fastest</source>
+        <translation>Nejrychlejší</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="84"/>
+        <source>Peaks falloff:</source>
+        <translation>Pokles špiček:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="126"/>
+        <source>Refresh rate:</source>
+        <translation>Obnovovací frekvence:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="140"/>
+        <source>50 FPS</source>
+        <translation>50 Hz</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="145"/>
+        <source>25 FPS</source>
+        <translation>25 Hz</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="150"/>
+        <source>10 FPS</source>
+        <translation>10 Hz</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="155"/>
+        <source>5 FPS</source>
+        <translation>5 Hz</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <source>Cells size:</source>
+        <translation>Velikost polí:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="223"/>
+        <source>Colors</source>
+        <translation>Barvy</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="229"/>
+        <source>Peaks:</source>
+        <translation>Špičky:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="261"/>
+        <source>Analyzer #1:</source>
+        <translation>Analyzátor #1:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="293"/>
+        <source>Background:</source>
+        <translation>Pozadí:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="325"/>
+        <source>Analyzer #2:</source>
+        <translation>Analyzátor #2:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="370"/>
+        <source>Analyzer #3:</source>
+        <translation>Analyzátor #3:</translation>
+    </message>
+</context>
+<context>
+    <name>VisualAnalyzerFactory</name>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="30"/>
+        <source>Analyzer Plugin</source>
+        <translation>Modul frekvenčního analyzátoru</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="49"/>
+        <source>About Analyzer Visual Plugin</source>
+        <translation>O modulu frekvenčního analyzátoru</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="50"/>
+        <source>Qmmp Analyzer Visual Plugin</source>
+        <translation>Modul frekvenčního analyzátoru pro Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="51"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+</TS>
