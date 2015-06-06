08e5d8abfe73f7585ab3c11486c31a0589b68908
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-06-06 17:05:10 +0000

    tagging qmmp-0.8.5

---
+++ tags/qmmp-0.8.5/src/plugins/Output/oss/translations/oss_plugin_gl_ES.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="gl_ES">
+<context>
+    <name>OutputOSSFactory</name>
+    <message>
+        <location filename="../outputossfactory.cpp" line="36"/>
+        <source>OSS Plugin</source>
+        <translation>Engadido OSS</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="56"/>
+        <source>About OSS Output Plugin</source>
+        <translation>Sobre o engadido OSS</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="57"/>
+        <source>Qmmp OSS Output Plugin</source>
+        <translation>Engadido OSS de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="58"/>
+        <source>Written by: Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</source>
+        <translation>Escrito por: Yuriy Zhuravlev &lt;slalkerg@gmail.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../outputossfactory.cpp" line="59"/>
+        <source>Based on code by: Brad Hughes &lt;bhughes@trolltech.com&gt;</source>
+        <translation>Baseado no código por: Brad Hughes &lt;bhughes@trolltech.com&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>OSS Plugin Settings</source>
+        <translation>Preferencias do engadido OSS</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="30"/>
+        <source>Device Settings</source>
+        <translation>Preferencias de dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="42"/>
+        <source>Audio device</source>
+        <translation>Dispositivo de audio</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="58"/>
+        <source>Mixer device</source>
+        <translation>Dispositivo mesturador</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="81"/>
+        <source>Advanced Settings</source>
+        <translation>Opcións avanzadas</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="93"/>
+        <source>Soundcard</source>
+        <translation>Tarxeta de son</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="157"/>
+        <source>Buffer time (ms):</source>
+        <translation>Tempo de búfer (ms):</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="167"/>
+        <source>Period time (ms):</source>
+        <translation>Tempo de periodo (ms):</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="197"/>
+        <source>PCM over Master</source>
+        <translation>PCM sobre Master</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="224"/>
+        <source>Cancel</source>
+        <translation>Cancelar</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="231"/>
+        <source>OK</source>
+        <translation>Feito</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-0.8.5/src/plugins/Output/pulseaudio/outputpulseaudio.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 2006-2012 by Ilya Kotov                                 *
+ *   forkotov02@hotmail.ru                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.         *
+ ***************************************************************************/
+
+#ifndef OUTPUTPULSEAUDIO_H
+#define OUTPUTPULSEAUDIO_H
+
+#include <QObject>
+extern "C"{
+#include <pulse/simple.h>
+}
+#include <qmmp/output.h>
+
+/**
+    @author Ilya Kotov <forkotov02@hotmail.ru>
+*/
+class OutputPulseAudio : public Output
+{
+public:
+    OutputPulseAudio();
+    ~OutputPulseAudio();
+
+    bool initialize(quint32, int, Qmmp::AudioFormat format);
+    //output api
+    qint64 latency();
+    qint64 writeAudio(unsigned char *data, qint64 maxSize);
+    void drain();
+    void reset();
+
+private:
+    // helper functions
+    void uninitialize();
+    pa_simple *m_connection;
+};
+
+#endif // OUTPUTPULSEAUDIO_H
