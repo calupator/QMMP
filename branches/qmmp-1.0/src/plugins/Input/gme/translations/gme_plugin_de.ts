c9748723ecec3ebd7275f910d40d65f4f56ef4c3
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-07-26 11:59:50 +0000

    creating special branch for qt5 port

---
+++ branches/qmmp-1.0/src/plugins/Input/gme/translations/gme_plugin_de.ts
@@ -0,0 +1,60 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de">
+<context>
+    <name>DecoderGmeFactory</name>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="49"/>
+        <source>GME Plugin</source>
+        <translation>GME-Modul</translation>
+    </message>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="52"/>
+        <source>Game Music Files</source>
+        <translation>Musikdateien aus Spielen (Game Music)</translation>
+    </message>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="116"/>
+        <source>About GME Audio Plugin</source>
+        <translation>Über GME-Audiomodul</translation>
+    </message>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="117"/>
+        <source>Qmmp GME Audio Plugin</source>
+        <translation>Qmmp GME-Audiomodul</translation>
+    </message>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="118"/>
+        <source>This plugin uses Game_Music_Emu library to play game music files</source>
+        <translation>Dieses Modul verwendet die Bibliothek „Game_Music_Emu“, um Musikdateien aus Spielen abzuspielen</translation>
+    </message>
+    <message>
+        <location filename="../decodergmefactory.cpp" line="119"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Geschrieben von: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>GME Plugin Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="34"/>
+        <source>Fadeout length:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="44"/>
+        <source>ms</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="57"/>
+        <source>Enable fadeout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
