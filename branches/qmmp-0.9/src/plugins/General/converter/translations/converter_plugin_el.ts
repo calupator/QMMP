2e55a5ef945b04fe08b3131644731f9a8ece55f5
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-09-03 17:28:55 +0000

    creating branch qmmp-0.9

---
+++ branches/qmmp-0.9/src/plugins/General/converter/translations/converter_plugin_el.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>Converter</name>
+    <message>
+        <location filename="../converter.cpp" line="169"/>
+        <source>Track: %1</source>
+        <translation>Κομμάτι: %1</translation>
+    </message>
+    <message>
+        <location filename="../converter.cpp" line="171"/>
+        <source>Preset: %1</source>
+        <translation>Προεπιλογή: %1</translation>
+    </message>
+    <message>
+        <location filename="../converter.cpp" line="266"/>
+        <source>Encoding...</source>
+        <translation>Κωδικοποίηση...</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterDialog</name>
+    <message>
+        <location filename="../converterdialog.ui" line="14"/>
+        <source>Converter Settings</source>
+        <translation>Ρυθμίσεις Μετατροπέα</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="29"/>
+        <source>Select tracks to convert:</source>
+        <translation>Επιλέξτε τα κομμάτια προς μετατροπή:</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="43"/>
+        <source>Output directory:</source>
+        <translation>Κατάλογος εξόδου:</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="53"/>
+        <source>Output file name:</source>
+        <translation>Όνομα αρχείου εξόδου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="63"/>
+        <source>Preset:</source>
+        <translation>Προεπιλογή:</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="73"/>
+        <source>Overwrite existing files</source>
+        <translation>Αντικατάσταση των υπαρχόντων αρχείων</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="90"/>
+        <source>Choose a directory</source>
+        <translation>Επιλογή ενός καταλόγου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="110"/>
+        <source>Artist</source>
+        <translation>Καλλιτέχνης</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="111"/>
+        <source>Album</source>
+        <translation>Άλμπουμ</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="112"/>
+        <source>Album Artist</source>
+        <translation>Καλλιτέχνης άλμπουμ</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="113"/>
+        <source>Title</source>
+        <translation>Τίτλος</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="114"/>
+        <source>Track Number</source>
+        <translation>Αριθμός κομματιού</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="115"/>
+        <source>Two-digit Track Number</source>
+        <translation>Διψήφιος αριθμός κομματιού</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="120"/>
+        <source>Disc Number</source>
+        <translation>Αριθμός δίσκου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="121"/>
+        <source>File Name</source>
+        <translation>Όνομα αρχείου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="122"/>
+        <source>File Path</source>
+        <translation>Διαδρομή αρχείου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="132"/>
+        <source>Create a Copy</source>
+        <translation>Δημιουργία αντιγράφου</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="116"/>
+        <source>Genre</source>
+        <translation>Είδος</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="117"/>
+        <source>Comment</source>
+        <translation>Σχόλιο</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="118"/>
+        <source>Composer</source>
+        <translation>Συνθέτης</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="119"/>
+        <source>Duration</source>
+        <translation>Διάρκεια</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="123"/>
+        <source>Year</source>
+        <translation>Έτος</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="124"/>
+        <source>Condition</source>
+        <translation>Συνθήκη</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="130"/>
+        <source>Create</source>
+        <translation>Δημιουργία</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="131"/>
+        <source>Edit</source>
+        <translation>Επεξεργασία</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="133"/>
+        <source>Delete</source>
+        <translation>Διαγραφή</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterFactory</name>
+    <message>
+        <location filename="../converterfactory.cpp" line="28"/>
+        <source>Converter Plugin</source>
+        <translation>Πρόσθετο Μετατροπέας</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="49"/>
+        <source>About Converter Plugin</source>
+        <translation>Σχετικά με το πρόσθετο Μετατροπέας</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="50"/>
+        <source>Qmmp Converter Plugin</source>
+        <translation>Qmmp πρόσθετο Μετατροπέας</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="51"/>
+        <source>This plugin converts supported audio files to other file formats using external command-line encoders</source>
+        <translation>Αυτό το πρόσθετο μετατρέπει υποστηριζόμενους τύπους αρχείων ήχου σε άλλες μορφές χρησιμοποιώντας κωδικοποιητές της γραμμής εντολών </translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Γράφτηκε από τον: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterHelper</name>
+    <message>
+        <location filename="../converterhelper.cpp" line="35"/>
+        <source>Convert</source>
+        <translation>Μετατροπή</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="36"/>
+        <source>Meta+C</source>
+        <translation>Meta+C</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="42"/>
+        <source>Converting...</source>
+        <translation>Μετατρέπεται...</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="43"/>
+        <source>Cancel</source>
+        <translation>Ακύρωση</translation>
+    </message>
+</context>
+<context>
+    <name>PresetEditor</name>
+    <message>
+        <location filename="../preseteditor.ui" line="14"/>
+        <source>Preset Editor</source>
+        <translation>Επεξεργαστής προεπιλογών</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="29"/>
+        <source>General</source>
+        <translation>Γενικά</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="35"/>
+        <source>Name:</source>
+        <translation>Όνομα:</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="45"/>
+        <source>Extension:</source>
+        <translation>Επέκταση:</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="58"/>
+        <source>Command</source>
+        <translation>Εντολή</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="77"/>
+        <source>Options</source>
+        <translation>Επιλογές</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="83"/>
+        <source>Write tags</source>
+        <translation>Εγγραφή ετικετών</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="90"/>
+        <source>Convert to 16 bit</source>
+        <translation>Μετατροπή σε 16bit</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="37"/>
+        <source>%1 (Read Only)</source>
+        <translation>%1 (Ανάγνωση μόνο)</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="70"/>
+        <source>Output file</source>
+        <translation>Αρχείο εξόδου</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="71"/>
+        <source>Input file</source>
+        <translation>Αρχείο εισόδου</translation>
+    </message>
+</context>
+</TS>
