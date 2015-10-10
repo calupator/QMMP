ed40f81acd7718bac79af9c7a17dd2d97a17b836
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-10-10 07:50:27 +0000

    tagging qmmp-plugin-pack-0.9.2

---
+++ tags/qmmp-plugin-pack-0.9.2/src/Input/ffap/translations/ffap_plugin_he.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="he">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="48"/>
+        <source>FFap Plugin</source>
+        <translation>תוספת FFap</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="50"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation>קבצי Monkey&apos;s Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="153"/>
+        <source>About FFap Audio Plugin</source>
+        <translation>אודות תוספת שמע FFap</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="154"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>תוספת שמע FFap של Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="155"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation>תוספת זו מספקת תמיכה לקבצי Monkey&apos;s Audio ‏(APE)</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="156"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>חוברה על ידי: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;‎</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="157"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation>מבוססת על קוד מן deadbeef, FFmpeg וגם rockbox</translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="60"/>
+        <source>Length</source>
+        <translation>אריכות</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="61"/>
+        <source>Sample rate</source>
+        <translation>שיעור דגימה</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="61"/>
+        <source>Hz</source>
+        <translation>הרץ</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="62"/>
+        <source>Channels</source>
+        <translation>ערוצים</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="63"/>
+        <source>Bitrate</source>
+        <translation>שיעור סיביות</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="63"/>
+        <source>kbps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>File size</source>
+        <translation>גודל קובץ</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>KB</source>
+        <translation>ק״ב</translation>
+    </message>
+</context>
+</TS>
