b510ce720de6aaf72127cf239a57bb2bf1f67dee
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-11-14 09:00:17 +0000

    tagging qmmp-0.9.4

---
+++ tags/qmmp-0.9.4/src/plugins/Visual/analyzer/translations/analyzer_plugin_fr.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>Analyzer</name>
+    <message>
+        <location filename="../analyzer.cpp" line="47"/>
+        <source>Qmmp Analyzer</source>
+        <translation>Analyseur Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="364"/>
+        <source>Peaks</source>
+        <translation>Pics</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="367"/>
+        <source>Refresh Rate</source>
+        <translation>Taux de rafraîchissement</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="370"/>
+        <source>50 fps</source>
+        <translation>50 ips</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="371"/>
+        <source>25 fps</source>
+        <translation>25 ips</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="372"/>
+        <source>10 fps</source>
+        <translation>10 ips</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="373"/>
+        <source>5 fps</source>
+        <translation>5 ips</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="380"/>
+        <source>Analyzer Falloff</source>
+        <translation>Retombée de l&apos;analyseur</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="383"/>
+        <location filename="../analyzer.cpp" line="397"/>
+        <source>Slowest</source>
+        <translation>Très lente</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="384"/>
+        <location filename="../analyzer.cpp" line="398"/>
+        <source>Slow</source>
+        <translation>Lente</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="385"/>
+        <location filename="../analyzer.cpp" line="399"/>
+        <source>Medium</source>
+        <translation>Moyenne</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="386"/>
+        <location filename="../analyzer.cpp" line="400"/>
+        <source>Fast</source>
+        <translation>Rapide</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="387"/>
+        <location filename="../analyzer.cpp" line="401"/>
+        <source>Fastest</source>
+        <translation>Très rapide</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="394"/>
+        <source>Peaks Falloff</source>
+        <translation>Retombée des pics</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="408"/>
+        <source>&amp;Full Screen</source>
+        <translation>&amp;Plein écran</translation>
+    </message>
+    <message>
+        <location filename="../analyzer.cpp" line="408"/>
+        <source>F</source>
+        <translation>F</translation>
+    </message>
+</context>
+<context>
+    <name>ColorWidget</name>
+    <message>
+        <location filename="../colorwidget.cpp" line="37"/>
+        <source>Select Color</source>
+        <translation>Sélectionner couleur</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Analyzer Plugin Settings</source>
+        <translation>Configuration du greffon d&apos;analyse</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>General</source>
+        <translation>Général</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <source>Cells size:</source>
+        <translation>Taille des cellules :</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="101"/>
+        <source>Colors</source>
+        <translation>Couleurs</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="107"/>
+        <source>Peaks:</source>
+        <translation>Pics :</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="139"/>
+        <source>Analyzer #1:</source>
+        <translation>Analyseur n°1 :</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="171"/>
+        <source>Background:</source>
+        <translation>Arrière-plan :</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="203"/>
+        <source>Analyzer #2:</source>
+        <translation>Analyseur n°2 :</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="248"/>
+        <source>Analyzer #3:</source>
+        <translation>Analyseur n°3 :</translation>
+    </message>
+</context>
+<context>
+    <name>VisualAnalyzerFactory</name>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="30"/>
+        <source>Analyzer Plugin</source>
+        <translation>Greffon d&apos;analyse</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="49"/>
+        <source>About Analyzer Visual Plugin</source>
+        <translation>À propos du greffon visuel d&apos;analyse</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="50"/>
+        <source>Qmmp Analyzer Visual Plugin</source>
+        <translation>Greffon visuel d&apos;analyse pour Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../visualanalyzerfactory.cpp" line="51"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Écrit par : Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+</TS>
