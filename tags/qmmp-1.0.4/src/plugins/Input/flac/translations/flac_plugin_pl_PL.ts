f711c4a7ba211e8e84c2c1fb7d8ce5db3786afb0
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2015-12-21 06:28:38 +0000

    tagging qmmp-1.0.4

---
+++ tags/qmmp-1.0.4/src/plugins/Input/flac/translations/flac_plugin_pl_PL.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pl_PL">
+<context>
+    <name>DecoderFLACFactory</name>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="61"/>
+        <source>FLAC Plugin</source>
+        <translation>Wtyczka FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="63"/>
+        <source>FLAC Files</source>
+        <translation>Pliki FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="204"/>
+        <source>About FLAC Audio Plugin</source>
+        <translation>O wtyczce FLAC Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="205"/>
+        <source>Qmmp FLAC Audio Plugin</source>
+        <translation>Wtyczka FLAC Audio dla Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="206"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>FLACMetaDataModel</name>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="80"/>
+        <source>Length</source>
+        <translation>Długość</translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="81"/>
+        <source>Sample rate</source>
+        <translation>Próbkowanie</translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="81"/>
+        <source>Hz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="82"/>
+        <source>Channels</source>
+        <translation>Kanały</translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="83"/>
+        <source>Bitrate</source>
+        <translation>Szybkość transmisji</translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="83"/>
+        <source>kbps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="85"/>
+        <source>File size</source>
+        <translation>Wielkość pliku</translation>
+    </message>
+    <message>
+        <location filename="../flacmetadatamodel.cpp" line="85"/>
+        <source>KB</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
