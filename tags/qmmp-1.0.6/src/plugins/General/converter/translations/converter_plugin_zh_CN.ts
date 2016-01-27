c769efd30b8785ea0db437d54fdf41ea05113beb
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-01-27 06:11:55 +0000

    tagging qmmp-1.0.6

---
+++ tags/qmmp-1.0.6/src/plugins/General/converter/translations/converter_plugin_zh_CN.ts
@@ -0,0 +1,264 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_CN">
+<context>
+    <name>Converter</name>
+    <message>
+        <location filename="../converter.cpp" line="169"/>
+        <source>Track: %1</source>
+        <translation>曲目:%1</translation>
+    </message>
+    <message>
+        <location filename="../converter.cpp" line="171"/>
+        <source>Preset: %1</source>
+        <translation> 预设:%1</translation>
+    </message>
+    <message>
+        <location filename="../converter.cpp" line="266"/>
+        <source>Encoding...</source>
+        <translation>编码......</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterDialog</name>
+    <message>
+        <location filename="../converterdialog.ui" line="14"/>
+        <source>Converter Settings</source>
+        <translation>转换设置</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="29"/>
+        <source>Select tracks to convert:</source>
+        <translation>选在曲目进行转换</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="43"/>
+        <source>Output directory:</source>
+        <translation>输出文件夹：</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="53"/>
+        <source>Output file name:</source>
+        <translation>输入文档名称：</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="63"/>
+        <source>Preset:</source>
+        <translation>预设：</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.ui" line="73"/>
+        <source>Overwrite existing files</source>
+        <translation>覆盖已有文件</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="90"/>
+        <source>Choose a directory</source>
+        <translation>选择文件夹</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="110"/>
+        <source>Artist</source>
+        <translation>艺术家</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="111"/>
+        <source>Album</source>
+        <translation>专辑</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="112"/>
+        <source>Album Artist</source>
+        <translation>专辑艺术家</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="113"/>
+        <source>Title</source>
+        <translation>题头</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="114"/>
+        <source>Track Number</source>
+        <translation>单曲号</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="115"/>
+        <source>Two-digit Track Number</source>
+        <translation>两位单曲号</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="120"/>
+        <source>Disc Number</source>
+        <translation>碟片编号</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="121"/>
+        <source>File Name</source>
+        <translation>文件名</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="122"/>
+        <source>File Path</source>
+        <translation>文件路径</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="132"/>
+        <source>Create a Copy</source>
+        <translation>创建一个副本</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="116"/>
+        <source>Genre</source>
+        <translation>风格</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="117"/>
+        <source>Comment</source>
+        <translation>评论</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="118"/>
+        <source>Composer</source>
+        <translation>曲作者</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="119"/>
+        <source>Duration</source>
+        <translation>长度</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="123"/>
+        <source>Year</source>
+        <translation>年份</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="124"/>
+        <source>Condition</source>
+        <translation>条件</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="130"/>
+        <source>Create</source>
+        <translation>创建</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="131"/>
+        <source>Edit</source>
+        <translation>编辑</translation>
+    </message>
+    <message>
+        <location filename="../converterdialog.cpp" line="133"/>
+        <source>Delete</source>
+        <translation>删除</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterFactory</name>
+    <message>
+        <location filename="../converterfactory.cpp" line="29"/>
+        <source>Converter Plugin</source>
+        <translation>转换器插件</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="50"/>
+        <source>About Converter Plugin</source>
+        <translation>关于转换器插件</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="51"/>
+        <source>Qmmp Converter Plugin</source>
+        <translation>Qmmp转换器插件</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="52"/>
+        <source>This plugin converts supported audio files to other file formats using external command-line encoders</source>
+        <translation>该插件使用外置命令行编码器转换支持格式的音频文件</translation>
+    </message>
+    <message>
+        <location filename="../converterfactory.cpp" line="54"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>作者：Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>ConverterHelper</name>
+    <message>
+        <location filename="../converterhelper.cpp" line="35"/>
+        <source>Convert</source>
+        <translation>转换</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="36"/>
+        <source>Meta+C</source>
+        <translation>Meta+C</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="42"/>
+        <source>Converting...</source>
+        <translation>转换中......</translation>
+    </message>
+    <message>
+        <location filename="../converterhelper.cpp" line="43"/>
+        <source>Cancel</source>
+        <translation>取消</translation>
+    </message>
+</context>
+<context>
+    <name>PresetEditor</name>
+    <message>
+        <location filename="../preseteditor.ui" line="14"/>
+        <source>Preset Editor</source>
+        <translation>预设编辑器</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="29"/>
+        <source>General</source>
+        <translation>总的</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="35"/>
+        <source>Name:</source>
+        <translation>姓名：</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="45"/>
+        <source>Extension:</source>
+        <translation>扩展名：</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="58"/>
+        <source>Command</source>
+        <translation>命令</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="77"/>
+        <source>Options</source>
+        <translation>选项</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="83"/>
+        <source>Write tags</source>
+        <translation>写书签</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.ui" line="90"/>
+        <source>Convert to 16 bit</source>
+        <translation>转换成16位</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="37"/>
+        <source>%1 (Read Only)</source>
+        <translation>%1 （只读）</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="70"/>
+        <source>Output file</source>
+        <translation>输出文件</translation>
+    </message>
+    <message>
+        <location filename="../preseteditor.cpp" line="71"/>
+        <source>Input file</source>
+        <translation>输入文件</translation>
+    </message>
+</context>
+</TS>
