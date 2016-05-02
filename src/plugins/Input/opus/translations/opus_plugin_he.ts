24d4eecd53c482c8a6dd5095d76784454afc595c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-02 10:03:50 +0000

    tagging qmmp-0.9.9

---
+++ src/plugins/Input/opus/translations/opus_plugin_he.ts
@@ -0,0 +1,80 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="he">
+<context>
+    <name>DecoderOpusFactory</name>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="47"/>
+        <source>Opus Plugin</source>
+        <translation>תוספת Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="50"/>
+        <source>Ogg Opus Files</source>
+        <translation>תוספת Ogg Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="124"/>
+        <source>About Opus Audio Plugin</source>
+        <translation>אודות תוספת שמע Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="125"/>
+        <source>Qmmp Opus Audio Plugin</source>
+        <translation>תוספת שמע Opus של Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="126"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>חוברה על ידי: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;‎</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="127"/>
+        <source>This plugin includes code from TagLib library</source>
+        <translation>תוספת זו כוללת קוד מתוך הספרייה TagLib</translation>
+    </message>
+</context>
+<context>
+    <name>OpusMetaDataModel</name>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="54"/>
+        <source>Length</source>
+        <translation>אריכות</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="55"/>
+        <source>Sample rate</source>
+        <translation>שיעור דגימה</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="55"/>
+        <source>Hz</source>
+        <translation>הרץ</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="56"/>
+        <source>Channels</source>
+        <translation>ערוצים</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="57"/>
+        <source>Bitrate</source>
+        <translation>שיעור סיביות</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="57"/>
+        <source>kbps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="58"/>
+        <source>File size</source>
+        <translation>גודל קובץ</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="58"/>
+        <source>KB</source>
+        <translation>ק״ב</translation>
+    </message>
+</context>
+</TS>
