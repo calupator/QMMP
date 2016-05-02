e94cf27a4c903b63fe7a2eabafb5a73dbc6b8077
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-02 12:50:57 +0000

    tagging qmmp-1.0.9

---
+++ src/plugins/Input/sndfile/translations/sndfile_plugin_kk.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="kk_KZ">
+<context>
+    <name>DecoderSndFileFactory</name>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="65"/>
+        <source>Sndfile Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="68"/>
+        <source>PCM Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="132"/>
+        <source>About Sndfile Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="133"/>
+        <source>Qmmp Sndfile Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="134"/>
+        <source>Compiled against</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodersndfilefactory.cpp" line="135"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
