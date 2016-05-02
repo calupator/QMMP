16f8515e44f9462d794f8972a5e92c762a5fb638
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-02 10:04:44 +0000

    tagging qmmp-1.0.8

---
+++ tags/qmmp-1.0.8/src/plugins/Input/opus/translations/opus_plugin_cs.ts
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<context>
+    <name>DecoderOpusFactory</name>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="50"/>
+        <source>Opus Plugin</source>
+        <translation>Modul Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="53"/>
+        <source>Ogg Opus Files</source>
+        <translation>Soubory Ogg Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="127"/>
+        <source>About Opus Audio Plugin</source>
+        <translation>O modulu Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="128"/>
+        <source>Qmmp Opus Audio Plugin</source>
+        <translation>Modul Qmmp Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="129"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Autor: Ilja Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>OpusMetaDataModel</name>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="51"/>
+        <source>Length</source>
+        <translation>Délka</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="52"/>
+        <source>Sample rate</source>
+        <translation>Vzorkovací frekvence</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="52"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="53"/>
+        <source>Channels</source>
+        <translation>Počet kanálů</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="54"/>
+        <source>Bitrate</source>
+        <translation>Datový tok</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="54"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="55"/>
+        <source>File size</source>
+        <translation>Velikost souboru</translation>
+    </message>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="55"/>
+        <source>KB</source>
+        <translation>KiB</translation>
+    </message>
+</context>
+</TS>
