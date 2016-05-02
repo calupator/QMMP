216ecf818f1dbc7329c3a0a4cefa819e046d7587
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-05-02 10:08:33 +0000

    tagging qmmp-plugin-pack-1.0.3

---
+++ src/Input/ffap/translations/ffap_plugin_nl.ts
@@ -0,0 +1,85 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="50"/>
+        <source>FFap Plugin</source>
+        <translation>FFap Module</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="52"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation>Monkey&apos;s Audio Bestanden</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="156"/>
+        <source>About FFap Audio Plugin</source>
+        <translation>Over de FFap Audio Module</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="157"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>FFap Audio Module voor Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="158"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation>Deze module faciliteert in het afspelen van Monkey&apos;s Audio (APE) bestanden</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="159"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Auteur: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="160"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation>Gebasseerd op de broncode van deadbeef, FFmpeg en rockbox</translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="60"/>
+        <source>Length</source>
+        <translation>Duur</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="61"/>
+        <source>Sample rate</source>
+        <translation>Sample frequentie</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="61"/>
+        <source>Hz</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="62"/>
+        <source>Channels</source>
+        <translation>Kanalen</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="63"/>
+        <source>Bitrate</source>
+        <translation>Bitsnelheid</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="63"/>
+        <source>kbps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>File size</source>
+        <translation>Bestandsgrootte</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="64"/>
+        <source>KB</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
