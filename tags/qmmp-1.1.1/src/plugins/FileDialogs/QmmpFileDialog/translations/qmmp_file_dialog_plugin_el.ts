88a5551030462d65633c31358f7a256f32bf3e5f
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2016-07-04 16:28:00 +0000

    tagging qmmp-1.1.1

---
+++ tags/qmmp-1.1.1/src/plugins/FileDialogs/QmmpFileDialog/translations/qmmp_file_dialog_plugin_el.ts
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>QmmpFileDialog</name>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="13"/>
+        <source>Add Files</source>
+        <translation>Προσθήκη αρχείων</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="37"/>
+        <source>Up</source>
+        <translation>Πάνω</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="40"/>
+        <location filename="../qmmpfiledialog.ui" line="53"/>
+        <location filename="../qmmpfiledialog.ui" line="72"/>
+        <location filename="../qmmpfiledialog.ui" line="97"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="50"/>
+        <source>List view</source>
+        <translation>Προβολή λίστας</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="69"/>
+        <source>Detailed view</source>
+        <translation>Προβολή λεπτομερειών</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="94"/>
+        <source>Close dialog on add</source>
+        <translation>Κλείσιμο του διαλόγου κατά την προσθήκη</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="177"/>
+        <source>File name:</source>
+        <translation>Όνομα αρχείου:</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="193"/>
+        <source>Add</source>
+        <translation>Προσθήκη</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="200"/>
+        <source>Files of type:</source>
+        <translation>Τύποι αρχείων:</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.ui" line="226"/>
+        <source>Close</source>
+        <translation>Κλείσιμο</translation>
+    </message>
+</context>
+<context>
+    <name>QmmpFileDialogFactory</name>
+    <message>
+        <location filename="../qmmpfiledialog.cpp" line="69"/>
+        <location filename="../qmmpfiledialog.cpp" line="79"/>
+        <source>Qmmp File Dialog</source>
+        <translation>Διάλογος αρχείων Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.cpp" line="78"/>
+        <source>About Qmmp File Dialog</source>
+        <translation>Σχετικά με το διάλογο αρχείων Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.cpp" line="80"/>
+        <source>Written by:
+Vladimir Kuznetsov &lt;vovanec@gmail.com&gt;
+Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</source>
+        <translation>Γράφτηκε από τους:
+Vladimir Kuznetsov &lt;vovanec@gmail.com&gt;
+Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialog.cpp" line="83"/>
+        <source>Some code is copied from the Qt library</source>
+        <translation>Έχουν χρησιμοποιηθεί αποσπάσματα κώδικα από τη βιβλιοθήκη Qt</translation>
+    </message>
+</context>
+<context>
+    <name>QmmpFileDialogImpl</name>
+    <message>
+        <location filename="../qmmpfiledialogimpl.cpp" line="250"/>
+        <source>Add</source>
+        <translation>Προσθήκη</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialogimpl.cpp" line="263"/>
+        <source>Save</source>
+        <translation>Αποθήκευση</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialogimpl.cpp" line="277"/>
+        <source>Directories</source>
+        <translation>Καταλόγοι</translation>
+    </message>
+    <message>
+        <location filename="../qmmpfiledialogimpl.cpp" line="446"/>
+        <source>%1 already exists.
+Do you want to replace it?</source>
+        <translation>Το %1 υπάρχει ήδη.
+Επιθυμείτε να το αντικαταστήσετε;</translation>
+    </message>
+</context>
+</TS>
