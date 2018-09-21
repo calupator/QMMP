73c986fd4b0787f71eb8c05f9dc3e72f0b6648b0
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2018-09-21 21:00:13 +0000

    tagging qmmp-plugin-pack-1.2.3

---
+++ tags/qmmp-plugin-pack-1.2.3/src/Engines/ffvideo/translations/ffvideo_plugin_it.ts
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it">
+<context>
+    <name>FFVideoFactory</name>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="42"/>
+        <source>FFmpeg Video Plugin</source>
+        <translation>Estensione video FFmpeg</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="47"/>
+        <source>Video Files</source>
+        <translation>File video</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="141"/>
+        <source>About FFVideo Plugin</source>
+        <translation>Informazioni sull&apos;estensione FFVideo</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="142"/>
+        <source>FFmpeg-based video plugin for Qmmp</source>
+        <translation>Estensione video basata su FFmpeg per Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="143"/>
+        <source>Compiled against:</source>
+        <translation>Compilato con:</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="153"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Autori: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>FFVideoMetaDataModel</name>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="52"/>
+        <source>Length</source>
+        <translation>Durata</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="53"/>
+        <source>File size</source>
+        <translation>Dimensione file</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="53"/>
+        <source>%1 KB</source>
+        <translation>%1 KB</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="54"/>
+        <source>Bitrate</source>
+        <translation>bit al secondo</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="54"/>
+        <location filename="../ffvideometadatamodel.cpp" line="62"/>
+        <source>%1 kbps</source>
+        <translation>%1 kbps</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="62"/>
+        <source>Audio bitrate</source>
+        <translation>Bit al secondo audio</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="63"/>
+        <source>Audio sample rate</source>
+        <translation>Frequenza di campionamento audio</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="63"/>
+        <source>%1 Hz</source>
+        <translation>%1 Hz</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="64"/>
+        <source>Audio channels</source>
+        <translation>Canali audio</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="70"/>
+        <source>Video size</source>
+        <translation>Dimensione video</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="71"/>
+        <source>Video bitrate</source>
+        <translation>bit al secondo video</translation>
+    </message>
+</context>
+<context>
+    <name>VideoWindow</name>
+    <message>
+        <location filename="../videowindow.cpp" line="37"/>
+        <source>FFmpeg Video</source>
+        <translation>Video FFmpeg</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="51"/>
+        <source>Alt+Return</source>
+        <translation>Alt+Invio</translation>
+    </message>
+</context>
+</TS>
