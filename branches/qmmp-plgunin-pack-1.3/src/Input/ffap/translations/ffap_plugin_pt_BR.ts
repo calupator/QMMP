2d49fdc9c76960d0a25f15a42c67d81e66812f7a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-02-09 16:28:49 +0000

    creating qmmp-plugin-pack-1.3 stable branch

---
+++ branches/qmmp-plgunin-pack-1.3/src/Input/ffap/translations/ffap_plugin_pt_BR.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pt_BR">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="43"/>
+        <source>FFap Plugin</source>
+        <translation>Plugin FFap</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="45"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation>Arquivos Monkey&apos;s Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="157"/>
+        <source>About FFap Audio Plugin</source>
+        <translation>Sobre o plugin FFap Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="158"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>Plugin Qmmp FFap Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="159"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation>Este plugin disponibiliza o suporte a arquivos Monkey&apos;s Audio (APE)</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="160"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="161"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation>Baseado no código de deadbeef, FFmpeg e rockbox</translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="67"/>
+        <source>Samples</source>
+        <translation>Amostras</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="68"/>
+        <source>Version</source>
+        <translation>Versão</translation>
+    </message>
+</context>
+</TS>
