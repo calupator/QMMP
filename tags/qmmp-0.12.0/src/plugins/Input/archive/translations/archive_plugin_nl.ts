f3895aa8356098520db0bae5094ec511ac60e33a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-02-09 17:03:35 +0000

    tagging qmmp-0.12.0

---
+++ tags/qmmp-0.12.0/src/plugins/Input/archive/translations/archive_plugin_nl.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl">
+<context>
+    <name>DecoderArchiveFactory</name>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="40"/>
+        <source>Archive Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="42"/>
+        <source>Archives</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="152"/>
+        <source>About Archive Reader Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="153"/>
+        <source>Qmmp Archive Reader Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="154"/>
+        <source>Compiled against %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="155"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Auteur: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-0.12.0/src/plugins/Input/vorbis/translations/vorbis_plugin_sr_BA.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="sr_BA">
+<context>
+    <name>DecoderVorbisFactory</name>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="47"/>
+        <source>Ogg Vorbis Plugin</source>
+        <translation>Огг Ворбис прикључак</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="50"/>
+        <source>Ogg Vorbis Files</source>
+        <translation>Огг Ворбис фајлови</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="130"/>
+        <source>About Ogg Vorbis Audio Plugin</source>
+        <translation>О Огг Ворбис прикључку</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="131"/>
+        <source>Qmmp Ogg Vorbis Audio Plugin</source>
+        <translation>Кумп Огг Ворбис прикључак</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="132"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Аутор: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="133"/>
+        <source>Source code based on mq3 project</source>
+        <translation>Заснован на mq3 пројекту</translation>
+    </message>
+</context>
+</TS>
