ab43d818a62c3609aed7b00b0869875da07e9438
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-05-04 19:02:51 +0000

    tagging qmmp-0.12.2

---
+++ src/plugins/General/scrobbler/translations/scrobbler_plugin_el.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>ScrobblerFactory</name>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="31"/>
+        <source>Scrobbler Plugin</source>
+        <translation>Πρόσθετο Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="51"/>
+        <source>About Scrobbler Plugin</source>
+        <translation>Σχετικά με το πρόσθετο Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="52"/>
+        <source>Qmmp AudioScrobbler Plugin</source>
+        <translation>Qmmp πρόσθετο AudioScrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Γράφτηκε από τον: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Scrobbler Plugin Settings</source>
+        <translation>Ρυθμίσεις πρόσθετου Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="20"/>
+        <source>Last.fm</source>
+        <translation>Last.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="31"/>
+        <location filename="../settingsdialog.ui" line="104"/>
+        <location filename="../settingsdialog.ui" line="160"/>
+        <source>Session:</source>
+        <translation>Συνεδρία:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <location filename="../settingsdialog.ui" line="117"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <source>Check</source>
+        <translation>Έλεγχος</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="63"/>
+        <location filename="../settingsdialog.ui" line="95"/>
+        <location filename="../settingsdialog.ui" line="151"/>
+        <source>Register new session</source>
+        <translation>Εγγραφή νέας συνεδρίας</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="73"/>
+        <source>Libre.fm</source>
+        <translation>Libre.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="129"/>
+        <source>ListenBrainz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="126"/>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Message</source>
+        <translation>Μήνυμα</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="127"/>
+        <source>1. Wait for browser startup</source>
+        <translation>1. Αναμονή εκκίνησης φυλλομετρητή</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>2. Allow Qmmp to scrobble tracks to your %1 account</source>
+        <translation>2. Επιτρέψτε στο Qmmp να κάνει αποθήκευση scrobble των κομματιών στο λογαριασμό σας στο %1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="129"/>
+        <source>3. Press &quot;OK&quot;</source>
+        <translation>3. Πιέστε «Εντάξει»</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Error</source>
+        <translation>Σφάλμα</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <source>Network error</source>
+        <translation>Σφάλμα δικτύου</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <source>Unable to register new session</source>
+        <translation>Αδύνατη η εγγραφή νέας συνεδρίας</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <source>New session has been received successfully</source>
+        <translation>Η νέα συνεδρία ελήφθη επιτυχώς</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Permission granted</source>
+        <translation>Η άδεια εκχωρήθηκε</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Permission denied</source>
+        <translation>Άρνηση χορήγησης άδειας</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/General/scrobbler/translations/scrobbler_plugin_gl_ES.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="gl_ES">
+<context>
+    <name>ScrobblerFactory</name>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="31"/>
+        <source>Scrobbler Plugin</source>
+        <translation>Engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="51"/>
+        <source>About Scrobbler Plugin</source>
+        <translation>Sobre o engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="52"/>
+        <source>Qmmp AudioScrobbler Plugin</source>
+        <translation>Engadio AudioScrobbler de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Escrito por: LLya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Scrobbler Plugin Settings</source>
+        <translation>Preferencias do engadido Scrobbler</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="20"/>
+        <source>Last.fm</source>
+        <translation>Last.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="31"/>
+        <location filename="../settingsdialog.ui" line="104"/>
+        <location filename="../settingsdialog.ui" line="160"/>
+        <source>Session:</source>
+        <translation>Sesión:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <location filename="../settingsdialog.ui" line="117"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <source>Check</source>
+        <translation>Comprobar</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="63"/>
+        <location filename="../settingsdialog.ui" line="95"/>
+        <location filename="../settingsdialog.ui" line="151"/>
+        <source>Register new session</source>
+        <translation>Rexistrar nova sesión</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="73"/>
+        <source>Libre.fm</source>
+        <translation>Libre.fm</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="129"/>
+        <source>ListenBrainz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="126"/>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Message</source>
+        <translation>Mensaxe</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="127"/>
+        <source>1. Wait for browser startup</source>
+        <translation>1. Esperar a que o navegador inicie</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>2. Allow Qmmp to scrobble tracks to your %1 account</source>
+        <translation>2. Permitir a Qmmp &quot;scrobblear&quot; pistas a túa conta %1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="129"/>
+        <source>3. Press &quot;OK&quot;</source>
+        <translation>3. Prema &quot;OK&quot;</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Error</source>
+        <translation>Erro</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <source>Network error</source>
+        <translation>Erro de rede</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <source>Unable to register new session</source>
+        <translation>Non se puido rexistrar nova sesión</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <source>New session has been received successfully</source>
+        <translation>A nova sesión recibiuse correctamente</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Permission granted</source>
+        <translation>Permiso concedido</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Permission denied</source>
+        <translation>Permiso denegado</translation>
+    </message>
+</context>
+</TS>
