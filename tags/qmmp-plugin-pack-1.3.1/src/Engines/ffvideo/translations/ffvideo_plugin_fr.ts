7c0522eb941e3482ac4e028d7affb0df88eac506
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-05-04 14:15:18 +0000

    tagging qmmp-plugin-pack-1.3.1

---
+++ tags/qmmp-plugin-pack-1.3.1/src/Engines/ffvideo/translations/ffvideo_plugin_fr.ts
@@ -0,0 +1,130 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>FFVideoFactory</name>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="41"/>
+        <source>FFmpeg Video Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="46"/>
+        <source>Video Files</source>
+        <translation>Fichiers vidéo</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="165"/>
+        <source>About FFVideo Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="166"/>
+        <source>FFmpeg-based video plugin for Qmmp</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="167"/>
+        <source>Compiled against:</source>
+        <translation>Compilé contre :</translation>
+    </message>
+    <message>
+        <location filename="../ffvideofactory.cpp" line="177"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Écrit par : Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>FFVideoMetaDataModel</name>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="50"/>
+        <source>File size</source>
+        <translation>Taille du fichier</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="51"/>
+        <source>Bitrate</source>
+        <translation>Taux de bit</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="70"/>
+        <source>Audio bitrate</source>
+        <translation>Débit Audio</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="71"/>
+        <source>Audio sample rate</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="50"/>
+        <source>KiB</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="51"/>
+        <location filename="../ffvideometadatamodel.cpp" line="70"/>
+        <location filename="../ffvideometadatamodel.cpp" line="79"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="71"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="72"/>
+        <source>Audio channels</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="78"/>
+        <source>Video size</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffvideometadatamodel.cpp" line="79"/>
+        <source>Video bitrate</source>
+        <translation>Débit vidéo</translation>
+    </message>
+</context>
+<context>
+    <name>VideoWindow</name>
+    <message>
+        <location filename="../videowindow.cpp" line="40"/>
+        <source>FFmpeg Video</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="45"/>
+        <source>&amp;Pause</source>
+        <translation>&amp;Pause</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="45"/>
+        <source>Space</source>
+        <translation>Espace</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="46"/>
+        <source>&amp;Stop</source>
+        <translation>&amp;Arrêter</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="46"/>
+        <source>V</source>
+        <translation>V</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="48"/>
+        <source>&amp;Fullscreen</source>
+        <translation>&amp;Plein écran</translation>
+    </message>
+    <message>
+        <location filename="../videowindow.cpp" line="48"/>
+        <source>F</source>
+        <translation>F</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-plugin-pack-1.3.1/src/Input/ffap/translations/ffap_plugin_it.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="43"/>
+        <source>FFap Plugin</source>
+        <translation>Estensione FFap</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="45"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation>File Monkey&apos;s Audio</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="157"/>
+        <source>About FFap Audio Plugin</source>
+        <translation>Informazioni sull&apos;estensione audio FFap</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="158"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation>Estensione audio FFap per Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="159"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation>Questa estensione fornisce il supporto per file Monkey&apos;s Audio (APE)</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="160"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Autori: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="161"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation>Basato sul codice di deadbeef, FFmpeg e rockbox</translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="67"/>
+        <source>Samples</source>
+        <translation>Campioni</translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="68"/>
+        <source>Version</source>
+        <translation>Versione</translation>
+    </message>
+</context>
+</TS>
