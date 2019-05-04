141586b09c7ee581873fb0369eee559d251a3259
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-05-04 19:03:40 +0000

    tagging qmmp-1.3.2

---
+++ src/plugins/Input/mpeg/translations/mpeg_plugin_fi.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>DecoderMPEGFactory</name>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="166"/>
+        <source>MPEG Plugin</source>
+        <translation>MPEG-liitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="169"/>
+        <source>MPEG Files</source>
+        <translation>MPEG-tiedostot</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="377"/>
+        <source>About MPEG Audio Plugin</source>
+        <translation>Tietoja - MPEG-ääniliitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="378"/>
+        <source>MPEG 1.0/2.0/2.5 layer 1/2/3 audio decoder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="379"/>
+        <source>Compiled against:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="388"/>
+        <source>mpg123, API version: %1</source>
+        <translation>mpg123, API-versio: %1</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="392"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Toteuttanut: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="393"/>
+        <source>Source code based on mq3 and madplay projects</source>
+        <translation>Lähdekoodi perustuu mq3- ja madplay-projektien koodiin</translation>
+    </message>
+</context>
+<context>
+    <name>MPEGMetaDataModel</name>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="65"/>
+        <location filename="../mpegmetadatamodel.cpp" line="68"/>
+        <location filename="../mpegmetadatamodel.cpp" line="71"/>
+        <location filename="../mpegmetadatamodel.cpp" line="74"/>
+        <source>Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="77"/>
+        <source>Protection</source>
+        <translation>Suojaus</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="78"/>
+        <source>Copyright</source>
+        <translation>Tekijänoikeus</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="79"/>
+        <source>Original</source>
+        <translation>Alkuperäinen</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>MPEG Plugin Settings</source>
+        <translation>MPEG-liitännäisen asetukset</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Decoder</source>
+        <translation>Purkaja</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <source>MAD</source>
+        <translation>MAD</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="48"/>
+        <source>MPG123</source>
+        <translation>MPG123</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="77"/>
+        <source>Tag Priority</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>First:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="109"/>
+        <location filename="../settingsdialog.ui" line="158"/>
+        <location filename="../settingsdialog.ui" line="207"/>
+        <source>ID3v1</source>
+        <translation>ID3v1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="114"/>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <location filename="../settingsdialog.ui" line="212"/>
+        <source>ID3v2</source>
+        <translation>ID3v2</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="119"/>
+        <location filename="../settingsdialog.ui" line="168"/>
+        <location filename="../settingsdialog.ui" line="217"/>
+        <source>APE</source>
+        <translation>APE</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="124"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <location filename="../settingsdialog.ui" line="222"/>
+        <source>Disabled</source>
+        <translation>Pois käytöstä</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="138"/>
+        <source>Second:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="187"/>
+        <source>Third:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="252"/>
+        <source>Encodings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="264"/>
+        <source>ID3v1 encoding:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="290"/>
+        <source>ID3v2 encoding:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
