b05da827b6cf1e0ecd27bd737ab0905afc43b8a5
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-06-15 16:16:47 +0000

    tagging qmmp-0.12.3

---
+++ tags/qmmp-0.12.3/src/plugins/Input/archive/translations/archive_plugin_es.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>DecoderArchiveFactory</name>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="40"/>
+        <source>Archive Plugin</source>
+        <translation>Módulo de archivo</translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="42"/>
+        <source>Archives</source>
+        <translation>Archivos</translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="160"/>
+        <source>About Archive Reader Plugin</source>
+        <translation>Acerca del Módulo Lector de Archivo</translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="161"/>
+        <source>Qmmp Archive Reader Plugin</source>
+        <translation>Módulo Lector de Archivo de Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="162"/>
+        <source>Compiled against %1</source>
+        <translation>Compilado contra %1</translation>
+    </message>
+    <message>
+        <location filename="../decoderarchivefactory.cpp" line="163"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Escrito por: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
