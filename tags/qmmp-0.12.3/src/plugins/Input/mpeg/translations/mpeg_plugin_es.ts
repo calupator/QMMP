34d3929033d848622e0cf5712e64c78bbce0f4e8
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-06-15 14:46:23 +0000

    tagging qmmp-0.12.3

---
+++ tags/qmmp-0.12.3/src/plugins/Input/mpeg/translations/mpeg_plugin_es.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="es">
+<context>
+    <name>DecoderMPEGFactory</name>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="174"/>
+        <source>MPEG Plugin</source>
+        <translation>Módulo MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="177"/>
+        <source>MPEG Files</source>
+        <translation>Archivos MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="393"/>
+        <source>About MPEG Audio Plugin</source>
+        <translation>Acerca del módulo de audio MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="394"/>
+        <source>MPEG 1.0/2.0/2.5 layer 1/2/3 audio decoder</source>
+        <translation>Decodificador de audio MPEG 1.0/2.0/2.5 capas 1/2/3</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="395"/>
+        <source>Compiled against:</source>
+        <translation>Compilado contra:</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="404"/>
+        <source>mpg123, API version: %1</source>
+        <translation>mpg123, versión de API: %1</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="408"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Escrito por: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="409"/>
+        <source>Source code based on mq3 and madplay projects</source>
+        <translation>Código fuente basado sobre proyectos mq3 y madplay</translation>
+    </message>
+</context>
+<context>
+    <name>MPEGMetaDataModel</name>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="71"/>
+        <location filename="../mpegmetadatamodel.cpp" line="74"/>
+        <location filename="../mpegmetadatamodel.cpp" line="77"/>
+        <location filename="../mpegmetadatamodel.cpp" line="80"/>
+        <source>Mode</source>
+        <translation>Modo</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="83"/>
+        <source>Protection</source>
+        <translation>Protección</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="84"/>
+        <source>Copyright</source>
+        <translation>Copyright</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="85"/>
+        <source>Original</source>
+        <translation>Original</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>MPEG Plugin Settings</source>
+        <translation>Configuración del módulo MPEG</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Decoder</source>
+        <translation>Decodificador</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <source>MAD</source>
+        <translation>MAD</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="48"/>
+        <source>MPG123</source>
+        <translation>MPG123</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="77"/>
+        <source>Tag Priority</source>
+        <translation>Prioridad de etiquetas</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>First:</source>
+        <translation>Primera:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="109"/>
+        <location filename="../settingsdialog.ui" line="158"/>
+        <location filename="../settingsdialog.ui" line="207"/>
+        <source>ID3v1</source>
+        <translation>ID3v1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="114"/>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <location filename="../settingsdialog.ui" line="212"/>
+        <source>ID3v2</source>
+        <translation>ID3v2</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="119"/>
+        <location filename="../settingsdialog.ui" line="168"/>
+        <location filename="../settingsdialog.ui" line="217"/>
+        <source>APE</source>
+        <translation>APE</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="124"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <location filename="../settingsdialog.ui" line="222"/>
+        <source>Disabled</source>
+        <translation>Deshabilitado</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="138"/>
+        <source>Second:</source>
+        <translation>Segunda:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="187"/>
+        <source>Third:</source>
+        <translation>Tercera:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="252"/>
+        <source>Encodings</source>
+        <translation>Codificaciones</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="264"/>
+        <source>ID3v1 encoding:</source>
+        <translation>Codificación ID3v1:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="290"/>
+        <source>ID3v2 encoding:</source>
+        <translation>Codificación ID3v2:</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-0.12.3/src/plugins/Input/vorbis/translations/vorbis_plugin_pt.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt">
+<context>
+    <name>DecoderVorbisFactory</name>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="47"/>
+        <source>Ogg Vorbis Plugin</source>
+        <translation>Suplemento Ogg Vorbis</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="50"/>
+        <source>Ogg Vorbis Files</source>
+        <translation>Ficheiros Ogg Vorbis</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="130"/>
+        <source>About Ogg Vorbis Audio Plugin</source>
+        <translation>Sobre o suplemento Ogg Vorbis</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="131"/>
+        <source>Qmmp Ogg Vorbis Audio Plugin</source>
+        <translation>Suplemento Qmmp Ogg Vorbis</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="132"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Desenvolvido por: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="133"/>
+        <source>Source code based on mq3 project</source>
+        <translation>Baseado no código fonte do projeto mq3</translation>
+    </message>
+</context>
+</TS>
