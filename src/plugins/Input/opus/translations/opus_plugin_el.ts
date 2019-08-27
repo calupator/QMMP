1dc9da3a49afca386fa17dc30139eb6452d3978d
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-08-27 19:31:29 +0000

    tagging qmmp-0.12.4

---
+++ src/plugins/Input/opus/translations/opus_plugin_el.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="el">
+<context>
+    <name>DecoderOpusFactory</name>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="43"/>
+        <source>Opus Plugin</source>
+        <translation>Πρόσθετο Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="46"/>
+        <source>Ogg Opus Files</source>
+        <translation>Αρχεία Ogg Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="122"/>
+        <source>About Opus Audio Plugin</source>
+        <translation>Σχετικά με το πρόσθετο ήχου Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="123"/>
+        <source>Qmmp Opus Audio Plugin</source>
+        <translation>Qmmp πρόσθετο ήχου Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="124"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Γράφτηκε από τον: Ilya Kotov &lt;forkotov02@hotmail.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="125"/>
+        <source>This plugin includes code from TagLib library</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpusMetaDataModel</name>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="50"/>
+        <source>Version</source>
+        <translation>Έκδοση</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/Input/vorbis/translations/vorbis_plugin_ja.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ja_JP">
+<context>
+    <name>DecoderVorbisFactory</name>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="47"/>
+        <source>Ogg Vorbis Plugin</source>
+        <translation>Ogg Vorbis プラグイン</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="50"/>
+        <source>Ogg Vorbis Files</source>
+        <translation>Ogg Vorbis ファイル</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="130"/>
+        <source>About Ogg Vorbis Audio Plugin</source>
+        <translation>Ogg Vorbis 音響プラグインについて</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="131"/>
+        <source>Qmmp Ogg Vorbis Audio Plugin</source>
+        <translation>QMMP Ogg Vorbis 音響プラグイン</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="132"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>制作: Илья Котов (Ilya Kotov) &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodervorbisfactory.cpp" line="133"/>
+        <source>Source code based on mq3 project</source>
+        <translation>ソースコードは mq3 プロジェクトから流用</translation>
+    </message>
+</context>
+</TS>
