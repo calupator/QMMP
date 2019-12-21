4d29b6bac2f8b110b69e2bb302628caf88495fb8
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2019-12-21 18:44:11 +0000

    tagging qmmp-1.3.5

---
+++ tags/qmmp-1.3.5/src/plugins/General/streambrowser/translations/streambrowser_plugin_nl.ts
@@ -0,0 +1,181 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>EditStreamDialog</name>
+    <message>
+        <location filename="../editstreamdialog.ui" line="14"/>
+        <source>Edit Stream</source>
+        <translation>Stream bewerken</translation>
+    </message>
+    <message>
+        <location filename="../editstreamdialog.ui" line="34"/>
+        <source>URL:</source>
+        <translation>URL:</translation>
+    </message>
+    <message>
+        <location filename="../editstreamdialog.ui" line="44"/>
+        <source>Name:</source>
+        <translation>Naam:</translation>
+    </message>
+    <message>
+        <location filename="../editstreamdialog.ui" line="54"/>
+        <source>Genre:</source>
+        <translation>Genre:</translation>
+    </message>
+    <message>
+        <location filename="../editstreamdialog.ui" line="64"/>
+        <source>Bitrate:</source>
+        <translation>Bitsnelheid:</translation>
+    </message>
+    <message>
+        <location filename="../editstreamdialog.ui" line="74"/>
+        <source>Type:</source>
+        <translation>Type:</translation>
+    </message>
+</context>
+<context>
+    <name>StreamBrowser</name>
+    <message>
+        <location filename="../streambrowser.cpp" line="33"/>
+        <source>Add Stream</source>
+        <translation>Stream toevoegen</translation>
+    </message>
+    <message>
+        <location filename="../streambrowser.cpp" line="35"/>
+        <source>Ctrl+U</source>
+        <translation>Ctrl+U</translation>
+    </message>
+</context>
+<context>
+    <name>StreamBrowserFactory</name>
+    <message>
+        <location filename="../streambrowserfactory.cpp" line="29"/>
+        <source>Stream Browser Plugin</source>
+        <translation>Radio Navigator Module</translation>
+    </message>
+    <message>
+        <location filename="../streambrowserfactory.cpp" line="50"/>
+        <source>About Stream Browser Plugin</source>
+        <translation>Over de Radio Navigator Module</translation>
+    </message>
+    <message>
+        <location filename="../streambrowserfactory.cpp" line="51"/>
+        <source>Qmmp Stream Browser Plugin</source>
+        <translation>Radio Navigator Module voor Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../streambrowserfactory.cpp" line="52"/>
+        <source>This plugin allows one to add stream from IceCast stream directory</source>
+        <translation>Deze module faciliteert in het toevoegen van radiostations uit de IceCast verzameling</translation>
+    </message>
+    <message>
+        <location filename="../streambrowserfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Auteur: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>StreamWindow</name>
+    <message>
+        <location filename="../streamwindow.ui" line="14"/>
+        <source>Stream Browser</source>
+        <translation>Radio Navigator</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.ui" line="31"/>
+        <source>Filter:</source>
+        <translation>Filter:</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.ui" line="47"/>
+        <source>Favorites</source>
+        <translation>Favorieten</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.ui" line="73"/>
+        <source>IceCast</source>
+        <translation>IceCast</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.ui" line="102"/>
+        <source>Add</source>
+        <translation>Voeg to</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.ui" line="109"/>
+        <source>Update</source>
+        <translation>Bijwerken</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="56"/>
+        <location filename="../streamwindow.cpp" line="74"/>
+        <source>Name</source>
+        <translation>Naam</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="57"/>
+        <location filename="../streamwindow.cpp" line="75"/>
+        <source>Genre</source>
+        <translation>Genre</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="58"/>
+        <location filename="../streamwindow.cpp" line="76"/>
+        <source>Bitrate</source>
+        <translation>Bitsnelheid</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="59"/>
+        <location filename="../streamwindow.cpp" line="77"/>
+        <source>Format</source>
+        <translation>Formaat</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="132"/>
+        <source>&amp;Add to favorites</source>
+        <translation>&amp;Voeg toe aan favorieten</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="135"/>
+        <source>&amp;Add to playlist</source>
+        <translation>&amp;Voeg toe aan afspeellijst</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="138"/>
+        <source>&amp;Create</source>
+        <translation>&amp;Creëren</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="140"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Bewerk</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="143"/>
+        <source>&amp;Remove</source>
+        <translation>&amp;Verwijder</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="153"/>
+        <source>Done</source>
+        <translation>Klaar</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="156"/>
+        <location filename="../streamwindow.cpp" line="157"/>
+        <source>Error</source>
+        <translation>Fout</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="176"/>
+        <source>Receiving</source>
+        <translation>Bezig met ontvangen</translation>
+    </message>
+    <message>
+        <location filename="../streamwindow.cpp" line="273"/>
+        <source>Edit Stream</source>
+        <translation>Stream bewerken</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-1.3.5/src/plugins/Input/mpeg/translations/mpeg_plugin_he.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="he">
+<context>
+    <name>DecoderMPEGFactory</name>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="175"/>
+        <source>MPEG Plugin</source>
+        <translation>תוסף MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="178"/>
+        <source>MPEG Files</source>
+        <translation>קבצי MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="386"/>
+        <source>About MPEG Audio Plugin</source>
+        <translation>אודות תוסף שמע MPEG</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="387"/>
+        <source>MPEG 1.0/2.0/2.5 layer 1/2/3 audio decoder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="388"/>
+        <source>Compiled against:</source>
+        <translation>הודר כנגד:</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="397"/>
+        <source>mpg123, API version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="401"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="402"/>
+        <source>Source code based on mq3 and madplay projects</source>
+        <translation>קוד מקור מבוסס על המיזמים mq3 וגם madplay</translation>
+    </message>
+</context>
+<context>
+    <name>MPEGMetaDataModel</name>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="65"/>
+        <location filename="../mpegmetadatamodel.cpp" line="68"/>
+        <location filename="../mpegmetadatamodel.cpp" line="71"/>
+        <location filename="../mpegmetadatamodel.cpp" line="74"/>
+        <source>Mode</source>
+        <translation>מצב</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="77"/>
+        <source>Protection</source>
+        <translation>הגנה</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="78"/>
+        <source>Copyright</source>
+        <translation>זכויות יוצרים</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="79"/>
+        <source>Original</source>
+        <translation>מקורי</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>MPEG Plugin Settings</source>
+        <translation>הגדרות תוסף MPEG</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Decoder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <source>MAD</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="48"/>
+        <source>MPG123</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="77"/>
+        <source>Tag Priority</source>
+        <translation>עדיפות תגית</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>First:</source>
+        <translation>ראשונה:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="109"/>
+        <location filename="../settingsdialog.ui" line="158"/>
+        <location filename="../settingsdialog.ui" line="207"/>
+        <source>ID3v1</source>
+        <translation>ID3v1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="114"/>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <location filename="../settingsdialog.ui" line="212"/>
+        <source>ID3v2</source>
+        <translation>ID3v2</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="119"/>
+        <location filename="../settingsdialog.ui" line="168"/>
+        <location filename="../settingsdialog.ui" line="217"/>
+        <source>APE</source>
+        <translation>APE</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="124"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <location filename="../settingsdialog.ui" line="222"/>
+        <source>Disabled</source>
+        <translation>מנוטרלת</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="138"/>
+        <source>Second:</source>
+        <translation>שניה:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="187"/>
+        <source>Third:</source>
+        <translation>שלישית:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="252"/>
+        <source>Encodings</source>
+        <translation>קידודים</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="264"/>
+        <source>ID3v1 encoding:</source>
+        <translation>קידוד ID3v1:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="290"/>
+        <source>ID3v2 encoding:</source>
+        <translation>קידוד ID3v2:</translation>
+    </message>
+</context>
+</TS>
