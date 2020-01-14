ca56137bc9cf1d2e4ca2c85ff5e0a7c753d11469
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-01-14 17:03:40 +0000

    tagging qmmp-1.3.6

---
+++ tags/qmmp-1.3.6/src/plugins/CommandLineOptions/PlayListOption/translations/playlist_plugin_fr.ts
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name>PlayListOption</name>
+    <message>
+        <location filename="../playlistoption.cpp" line="33"/>
+        <source>Show playlist manipulation commands</source>
+        <translation>Montrer les commandes de manipulation de playlist</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="34"/>
+        <source>List all available playlists</source>
+        <translation>Montrer toutes les playlists disponibles</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="35"/>
+        <source>Show playlist content</source>
+        <translation>Montrer le contenu de la playlist</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="36"/>
+        <source>Play track &lt;track&gt; in playlist &lt;id&gt;</source>
+        <translation>Lire le morceau &lt;track&gt; dans la playlist &lt;id&gt;</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="37"/>
+        <source>Clear playlist</source>
+        <translation>Vider la playlist</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="38"/>
+        <source>Toggle playlist repeat</source>
+        <translation>Activer/Désactiver la répétition de playlist</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="39"/>
+        <source>Toggle playlist shuffle</source>
+        <translation>Activer/Désactiver la lecture aléatoire</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="40"/>
+        <source>Show playlist options</source>
+        <translation>Montrer les options de playlist</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="104"/>
+        <location filename="../playlistoption.cpp" line="126"/>
+        <location filename="../playlistoption.cpp" line="143"/>
+        <source>Invalid playlist ID</source>
+        <translation>Identifiant de playlist invalide</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="120"/>
+        <source>Invalid number of arguments</source>
+        <translation>Nombre d&apos;arguments invalide</translation>
+    </message>
+    <message>
+        <location filename="../playlistoption.cpp" line="130"/>
+        <source>Invalid track ID</source>
+        <translation>Identifiant de morceau invalide</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-1.3.6/src/plugins/General/scrobbler/translations/scrobbler_plugin_kk.ts
@@ -0,0 +1,131 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="kk_KZ">
+<context>
+    <name>ScrobblerFactory</name>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="31"/>
+        <source>Scrobbler Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="51"/>
+        <source>About Scrobbler Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="52"/>
+        <source>Qmmp AudioScrobbler Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../scrobblerfactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Scrobbler Plugin Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="20"/>
+        <source>Last.fm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="31"/>
+        <location filename="../settingsdialog.ui" line="104"/>
+        <location filename="../settingsdialog.ui" line="160"/>
+        <source>Session:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <location filename="../settingsdialog.ui" line="117"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <source>Check</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="63"/>
+        <location filename="../settingsdialog.ui" line="95"/>
+        <location filename="../settingsdialog.ui" line="151"/>
+        <source>Register new session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="73"/>
+        <source>Libre.fm</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="129"/>
+        <source>ListenBrainz</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="126"/>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="127"/>
+        <source>1. Wait for browser startup</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="128"/>
+        <source>2. Allow Qmmp to scrobble tracks to your %1 account</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="129"/>
+        <source>3. Press &quot;OK&quot;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <source>Network error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <source>Unable to register new session</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="152"/>
+        <source>New session has been received successfully</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="217"/>
+        <source>Permission granted</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Permission denied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="134"/>
+        <location filename="../settingsdialog.cpp" line="138"/>
+        <location filename="../settingsdialog.cpp" line="172"/>
+        <location filename="../settingsdialog.cpp" line="176"/>
+        <location filename="../settingsdialog.cpp" line="227"/>
+        <location filename="../settingsdialog.cpp" line="231"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
