9002f392ba8aa8d84b5a2d638e3532979b1e3361
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-01-14 17:03:40 +0000

    tagging qmmp-1.3.6

---
+++ src/plugins/General/trackchange/translations/trackchange_plugin_nl.ts
@@ -0,0 +1,68 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="nl">
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Track Change Plugin Settings</source>
+        <translation>Instellingen</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="32"/>
+        <location filename="../settingsdialog.ui" line="42"/>
+        <location filename="../settingsdialog.ui" line="52"/>
+        <location filename="../settingsdialog.ui" line="62"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="76"/>
+        <source>Command to run when Qmmp starts new track</source>
+        <translation>Opdracht die moet worden uitgevoerd als er een nieuw nummer wordt afgespeeld</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="83"/>
+        <source>Command to run toward to end of a track</source>
+        <translation>Opdracht die moet worden uitgevoerd aan het eind van een nummer</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="90"/>
+        <source>Command to run when Qmmp reaches the end of the playlist</source>
+        <translation>Opdracht die moet worden uitgevoerd aan het eind van de afspeellijst</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="97"/>
+        <source>Command to run when title changes (i.e. network streams title)</source>
+        <translation>Opdracht die moet worden uitgevoerd als de titel wijzigt (bijv. bij internetstreams)</translation>
+    </message>
+</context>
+<context>
+    <name>TrackChangeFactory</name>
+    <message>
+        <location filename="../trackchangefactory.cpp" line="30"/>
+        <source>Track Change Plugin</source>
+        <translation>Nummerveranderingsplug-in</translation>
+    </message>
+    <message>
+        <location filename="../trackchangefactory.cpp" line="50"/>
+        <source>About Track Change Plugin</source>
+        <translation>Over de Nummerveranderingsplug-in</translation>
+    </message>
+    <message>
+        <location filename="../trackchangefactory.cpp" line="51"/>
+        <source>Qmmp Track Change Plugin</source>
+        <translation>Nummerveranderingsplug-in voor Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../trackchangefactory.cpp" line="52"/>
+        <source>This plugin executes external command when current track is changed</source>
+        <translation>Deze plug-in voert externe opdrachten uit als een nummer verandert</translation>
+    </message>
+    <message>
+        <location filename="../trackchangefactory.cpp" line="53"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Auteur: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/Input/wavpack/translations/wavpack_plugin_lt.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
+<context>
+    <name>DecoderWavPackFactory</name>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="37"/>
+        <source>WavPack Plugin</source>
+        <translation>WavPack įskiepis</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="39"/>
+        <source>WavPack Files</source>
+        <translation>WavPack bylos</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="175"/>
+        <source>About WavPack Audio Plugin</source>
+        <translation>Apie WavPack audio įskiepį</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="176"/>
+        <source>Qmmp WavPack Audio Plugin</source>
+        <translation>Qmmp WavPack audio įskiepis</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="177"/>
+        <source>WavPack library version:</source>
+        <translation>WavPack bibliotekos versija:</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="179"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Sukurta: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>WavPackMetaDataModel</name>
+    <message>
+        <location filename="../wavpackmetadatamodel.cpp" line="73"/>
+        <source>Ratio</source>
+        <translation>Koeficientas</translation>
+    </message>
+    <message>
+        <location filename="../wavpackmetadatamodel.cpp" line="74"/>
+        <source>Version</source>
+        <translation>Versija</translation>
+    </message>
+</context>
+</TS>
