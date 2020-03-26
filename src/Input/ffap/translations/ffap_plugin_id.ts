32a9ec4d4b12a024e39dc5f8274f1621347595ce
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-03-26 17:19:43 +0000

    tagging qmmp-plugin-pack-0.12.2

---
+++ src/Input/ffap/translations/ffap_plugin_id.ts
@@ -0,0 +1,55 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="id_ID">
+<context>
+    <name>DecoderFFapFactory</name>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="46"/>
+        <source>FFap Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="48"/>
+        <source>Monkey&apos;s Audio Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="168"/>
+        <source>About FFap Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="169"/>
+        <source>Qmmp FFap Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="170"/>
+        <source>This plugin provides Monkey&apos;s Audio (APE) support</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="171"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderffapfactory.cpp" line="172"/>
+        <source>Based on code from deadbeef, FFmpeg and rockbox</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FFapMetaDataModel</name>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="80"/>
+        <source>Samples</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../ffapmetadatamodel.cpp" line="82"/>
+        <source>Version</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
