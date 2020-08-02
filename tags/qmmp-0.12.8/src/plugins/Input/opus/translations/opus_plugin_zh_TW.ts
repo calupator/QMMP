7a19355bf2590a8918830d84faa8b2997fc284a2
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-08-02 10:00:27 +0000

    tagging qmmp-0.12.8

---
+++ tags/qmmp-0.12.8/src/plugins/Input/opus/translations/opus_plugin_zh_TW.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>DecoderOpusFactory</name>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="43"/>
+        <source>Opus Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="46"/>
+        <source>Ogg Opus Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="129"/>
+        <source>About Opus Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="130"/>
+        <source>Qmmp Opus Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="131"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>撰寫：Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="132"/>
+        <source>This plugin includes code from TagLib library</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpusMetaDataModel</name>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="49"/>
+        <source>Version</source>
+        <translation>版本</translation>
+    </message>
+</context>
+</TS>
