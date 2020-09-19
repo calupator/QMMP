d7fe58b61614c263d5b37c790d1bc06c3efa745c
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-09-19 20:47:05 +0000

    tagging qmmp-1.4.2

---
+++ tags/qmmp-1.4.2/src/plugins/General/lyrics/translations/lyrics_plugin_it.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>Lyrics</name>
+    <message>
+        <location filename="../lyrics.cpp" line="33"/>
+        <source>View Lyrics</source>
+        <translation>Visualizza testi</translation>
+    </message>
+    <message>
+        <location filename="../lyrics.cpp" line="34"/>
+        <source>Ctrl+L</source>
+        <translation>Ctrl+L</translation>
+    </message>
+</context>
+<context>
+    <name>LyricsFactory</name>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="29"/>
+        <source>Lyrics Plugin</source>
+        <translation>Estensioni testi</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="49"/>
+        <source>About Lyrics Plugin</source>
+        <translation>Informazioni sull&apos;estensione testi</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="50"/>
+        <source>Qmmp Lyrics Plugin</source>
+        <translation>Estensioni testi per Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="51"/>
+        <source>This plugin retrieves lyrics from LyricWiki</source>
+        <translation>Estensione che permette di trovare testi di canzoni da LyricWiki</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Autori: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="53"/>
+        <source>Based on Ultimate Lyrics script by Vladimir Brkic &lt;vladimir_brkic@yahoo.com&gt;</source>
+        <translation>Basata sullo script Ultimate Lyrics di Vladimir Brkic &lt;vladimir_brkic@yahoo.com&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>LyricsWindow</name>
+    <message>
+        <location filename="../lyricswindow.ui" line="14"/>
+        <source>Lyrics Plugin</source>
+        <translation>Estensione testi</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="31"/>
+        <source>Provider:</source>
+        <translation>Fornitore:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="122"/>
+        <source>Album:</source>
+        <translation>Album:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="139"/>
+        <source>Track:</source>
+        <translation>Traccia:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="146"/>
+        <source>Artist:</source>
+        <translation>Artista:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="161"/>
+        <source>Year:</source>
+        <translation>Anno:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="115"/>
+        <source>Title:</source>
+        <translation>Titolo:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="115"/>
+        <source>&lt;h2&gt;%1 - %2&lt;/h2&gt;</source>
+        <translation>&lt;h2&gt;%1 - %2&lt;/h2&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="126"/>
+        <source>Not found</source>
+        <translation>Non trovato</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="136"/>
+        <source>Error: %1 - %2</source>
+        <translation>Errore: %1 - %2</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="149"/>
+        <source>Receiving</source>
+        <translation>Ricezione</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="204"/>
+        <source>cache</source>
+        <translation>cache</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Lyrics Plugin Settings</source>
+        <translation>Impostazioni dell&apos;estensione testi</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="29"/>
+        <source>Lyrics providers:</source>
+        <translation>Fornitori di testi:</translation>
+    </message>
+</context>
+<context>
+    <name>UltimateLyricsParser</name>
+    <message>
+        <location filename="../ultimatelyricsparser.cpp" line="119"/>
+        <source>%1 (line: %2)</source>
+        <translation>%1 (riga: %2)</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-1.4.2/src/plugins/Input/flac/translations/flac_plugin_uk_UA.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>DecoderFLACFactory</name>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="53"/>
+        <source>FLAC Plugin</source>
+        <translation>Модуль FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="55"/>
+        <source>FLAC Files</source>
+        <translation>Файли FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="210"/>
+        <source>About FLAC Audio Plugin</source>
+        <translation>Про аудіо-модуль FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="211"/>
+        <source>Qmmp FLAC Audio Plugin</source>
+        <translation>Аудіо-модуль FLAC для Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="212"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
