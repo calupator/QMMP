6cb088cdb605245a57a2b99f7fc6e66cc92bca9e
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-10-28 11:46:58 +0000

    tagging qmmp-0.12.9

---
+++ tags/qmmp-0.12.9/src/plugins/CommandLineOptions/UninstallOption/translations/uninstall_plugin_nl.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="nl">
+<context>
+    <name>UninstallOption</name>
+    <message>
+        <location filename="../uninstalloption.cpp" line="30"/>
+        <source>Restore the old file associations and clean up the registry</source>
+        <translation>Herstel de oorspronkelijke bestandstoewijzingen en schoon het register op</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-0.12.9/src/plugins/Input/opus/translations/opus_plugin_fr.ts
@@ -0,0 +1,45 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr">
+<context>
+    <name>DecoderOpusFactory</name>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="43"/>
+        <source>Opus Plugin</source>
+        <translation>Greffon Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="46"/>
+        <source>Ogg Opus Files</source>
+        <translation>Fichiers Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="129"/>
+        <source>About Opus Audio Plugin</source>
+        <translation>À propos du greffon audio Opus</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="130"/>
+        <source>Qmmp Opus Audio Plugin</source>
+        <translation>Greffon audio Opus pour Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="131"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Écrit par : Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decoderopusfactory.cpp" line="132"/>
+        <source>This plugin includes code from TagLib library</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>OpusMetaDataModel</name>
+    <message>
+        <location filename="../opusmetadatamodel.cpp" line="49"/>
+        <source>Version</source>
+        <translation>Version</translation>
+    </message>
+</context>
+</TS>
