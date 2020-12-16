e4aa7e4eebd9d0530b6ab669d35387ca5ce91f94
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-12-16 20:52:46 +0000

    tagging qmmp-1.4.3

---
+++ tags/qmmp-1.4.3/src/plugins/Input/flac/translations/flac_plugin_hu.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="hu">
+<context>
+    <name>DecoderFLACFactory</name>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="53"/>
+        <source>FLAC Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="55"/>
+        <source>FLAC Files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="210"/>
+        <source>About FLAC Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="211"/>
+        <source>Qmmp FLAC Audio Plugin</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="212"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
