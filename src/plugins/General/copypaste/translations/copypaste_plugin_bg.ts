538eec9b722b0c7faf9b252c3b3ae6d818ad169a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2020-12-16 20:52:46 +0000

    tagging qmmp-1.4.3

---
+++ src/plugins/General/copypaste/translations/copypaste_plugin_bg.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>CopyPaste</name>
+    <message>
+        <location filename="../copypaste.cpp" line="41"/>
+        <source>Cu&amp;t</source>
+        <translation>Изрязване</translation>
+    </message>
+    <message>
+        <location filename="../copypaste.cpp" line="42"/>
+        <source>Ctrl+X</source>
+        <translation>Ctrl+X</translation>
+    </message>
+    <message>
+        <location filename="../copypaste.cpp" line="43"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Копиране</translation>
+    </message>
+    <message>
+        <location filename="../copypaste.cpp" line="44"/>
+        <source>Ctrl+C</source>
+        <translation>Ctrl+C</translation>
+    </message>
+    <message>
+        <location filename="../copypaste.cpp" line="45"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Поставяне</translation>
+    </message>
+    <message>
+        <location filename="../copypaste.cpp" line="46"/>
+        <source>Ctrl+V</source>
+        <translation>Ctrl+V</translation>
+    </message>
+</context>
+<context>
+    <name>CopyPasteFactory</name>
+    <message>
+        <location filename="../copypastefactory.cpp" line="29"/>
+        <source>Copy/Paste Plugin</source>
+        <translation>приставка за копиране / поставяне</translation>
+    </message>
+    <message>
+        <location filename="../copypastefactory.cpp" line="49"/>
+        <source>About Copy/Paste Plugin</source>
+        <translation>З</translation>
+    </message>
+    <message>
+        <location filename="../copypastefactory.cpp" line="50"/>
+        <source>Qmmp Copy/Paste Plugin</source>
+        <translation>Относно приставката за копиране и поставяне</translation>
+    </message>
+    <message>
+        <location filename="../copypastefactory.cpp" line="51"/>
+        <source>This plugin allows one to copy selected tracks from one playlist to another</source>
+        <translation>На тази приставка е разрешено да копира избраната песен от една листа в друга</translation>
+    </message>
+    <message>
+        <location filename="../copypastefactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Написана от Илия Котов &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/Input/aac/translations/aac_plugin_bg.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bg">
+<context>
+    <name>DecoderAACFactory</name>
+    <message>
+        <location filename="../decoderaacfactory.cpp" line="39"/>
+        <source>AAC Plugin</source>
+        <translation>ААС приставка</translation>
+    </message>
+    <message>
+        <location filename="../decoderaacfactory.cpp" line="41"/>
+        <source>AAC Files</source>
+        <translation>ААС филтър</translation>
+    </message>
+    <message>
+        <location filename="../decoderaacfactory.cpp" line="103"/>
+        <source>About AAC Audio Plugin</source>
+        <translation>Относно ААС аудио приставката</translation>
+    </message>
+    <message>
+        <location filename="../decoderaacfactory.cpp" line="104"/>
+        <source>Qmmp AAC Audio Plugin</source>
+        <translation>Qmmp ААС аудио плъгин</translation>
+    </message>
+    <message>
+        <location filename="../decoderaacfactory.cpp" line="105"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Написана от Илия Котов &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+</TS>
