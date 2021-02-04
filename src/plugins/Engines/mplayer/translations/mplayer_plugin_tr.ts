6a61f2b3da5fc4316ac39173d5ab89caa1782ee2
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2021-02-04 19:03:39 +0000

    tagging qmmp-1.4.4

---
+++ src/plugins/Engines/mplayer/translations/mplayer_plugin_tr.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>MplayerEngineFactory</name>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="33"/>
+        <source>Mplayer Plugin</source>
+        <translation>Mplayer Eklentisi</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="36"/>
+        <source>Video Files</source>
+        <translation>Vidyo Dosyaları</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="83"/>
+        <source>About MPlayer Plugin</source>
+        <translation>MPlayer Eklentisi Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="84"/>
+        <source>Qmmp MPlayer Plugin</source>
+        <translation>Qmmp MPlayer Eklentisi</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="85"/>
+        <source>This plugin uses MPlayer as backend</source>
+        <translation>Bu eklenti arka uç olarak MPlayer kullanır</translation>
+    </message>
+    <message>
+        <location filename="../mplayerenginefactory.cpp" line="86"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Yazan: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>MplayerMetaDataModel</name>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
+        <source>Size</source>
+        <translation>Boyut</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="39"/>
+        <source>KiB</source>
+        <translation>KiB</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="65"/>
+        <source>Demuxer</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="67"/>
+        <source>Video format</source>
+        <translation>Vidyo biçimi</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="68"/>
+        <source>FPS</source>
+        <translation>FPS</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="69"/>
+        <source>Video codec</source>
+        <translation>Vidyo kodlaması</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="70"/>
+        <source>Aspect ratio</source>
+        <translation>En boy oranı</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="71"/>
+        <source>Video bitrate</source>
+        <translation>Vidyo bit hızı</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="71"/>
+        <location filename="../mplayermetadatamodel.cpp" line="76"/>
+        <source>kbps</source>
+        <translation>kbps</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="74"/>
+        <source>Audio codec</source>
+        <translation>Ses kodlaması</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="75"/>
+        <source>Sample rate</source>
+        <translation>Örnekleme oranı</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="75"/>
+        <source>Hz</source>
+        <translation>Hz</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="76"/>
+        <source>Audio bitrate</source>
+        <translation>Ses bit hızı</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="77"/>
+        <source>Channels</source>
+        <translation>Kanallar</translation>
+    </message>
+    <message>
+        <location filename="../mplayermetadatamodel.cpp" line="72"/>
+        <source>Resolution</source>
+        <translation>Çözünürlük</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>MPlayer Settings</source>
+        <translation>MPlayer Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="34"/>
+        <source>Video:</source>
+        <translation>Vidyo:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="48"/>
+        <source>Audio:</source>
+        <translation>Ses:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="62"/>
+        <source>Audio/video auto synchronization</source>
+        <translation>Ses/video otomatik senkronizasyon</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="72"/>
+        <source>Synchronization factor:</source>
+        <translation>Eşleştirme etmeni:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>Extra options:</source>
+        <translation>Fazladan Seçenekler:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="96"/>
+        <source>Extra command line options</source>
+        <translation>Fazladan komut satırı seçenekleri</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.cpp" line="29"/>
+        <location filename="../settingsdialog.cpp" line="37"/>
+        <location filename="../settingsdialog.cpp" line="46"/>
+        <location filename="../settingsdialog.cpp" line="47"/>
+        <location filename="../settingsdialog.cpp" line="61"/>
+        <location filename="../settingsdialog.cpp" line="62"/>
+        <source>default</source>
+        <translation>öntanımlı</translation>
+    </message>
+</context>
+</TS>
---
+++ src/plugins/General/lyrics/translations/lyrics_plugin_uk_UA.ts
@@ -0,0 +1,134 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="uk">
+<context>
+    <name>Lyrics</name>
+    <message>
+        <location filename="../lyrics.cpp" line="33"/>
+        <source>View Lyrics</source>
+        <translation>Огляд текстів</translation>
+    </message>
+    <message>
+        <location filename="../lyrics.cpp" line="34"/>
+        <source>Ctrl+L</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LyricsFactory</name>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="29"/>
+        <source>Lyrics Plugin</source>
+        <translation>Модуль текстів</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="49"/>
+        <source>About Lyrics Plugin</source>
+        <translation>Про модуль текстів</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="50"/>
+        <source>Qmmp Lyrics Plugin</source>
+        <translation>Модуль текстів для Qmmp</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="52"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Розробник: Ілля Котов &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="53"/>
+        <source>Based on Ultimate Lyrics script by Vladimir Brkic &lt;vladimir_brkic@yahoo.com&gt;</source>
+        <translation>Базується на Ultimate Lyrics script від Vladimir Brkic &lt;vladimir_brkic@yahoo.com&gt;</translation>
+    </message>
+    <message>
+        <location filename="../lyricsfactory.cpp" line="51"/>
+        <source>This plugin retrieves lyrics from LyricWiki</source>
+        <translation>Цей модуль отримує тексти з LyricWiki</translation>
+    </message>
+</context>
+<context>
+    <name>LyricsWindow</name>
+    <message>
+        <location filename="../lyricswindow.cpp" line="115"/>
+        <source>&lt;h2&gt;%1 - %2&lt;/h2&gt;</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="126"/>
+        <source>Not found</source>
+        <translation>Не знайдено</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="136"/>
+        <source>Error: %1 - %2</source>
+        <translation>Помилка: %1 - %2</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="149"/>
+        <source>Receiving</source>
+        <translation>Отримання</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.cpp" line="204"/>
+        <source>cache</source>
+        <translation>кеш</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="14"/>
+        <source>Lyrics Plugin</source>
+        <translation>Модуль текстів</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="31"/>
+        <source>Provider:</source>
+        <translation>Постачальник:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="122"/>
+        <source>Album:</source>
+        <translation>Альбом:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="139"/>
+        <source>Track:</source>
+        <translation>Трек:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="146"/>
+        <source>Artist:</source>
+        <translation>Виконавець:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="161"/>
+        <source>Year:</source>
+        <translation>Рік:</translation>
+    </message>
+    <message>
+        <location filename="../lyricswindow.ui" line="115"/>
+        <source>Title:</source>
+        <translation>Заголовок:</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>Lyrics Plugin Settings</source>
+        <translation>Налаштування модуля текстів</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="29"/>
+        <source>Lyrics providers:</source>
+        <translation>Постачальники текстів:</translation>
+    </message>
+</context>
+<context>
+    <name>UltimateLyricsParser</name>
+    <message>
+        <location filename="../ultimatelyricsparser.cpp" line="119"/>
+        <source>%1 (line: %2)</source>
+        <translation>%1 (рядок: %2)</translation>
+    </message>
+</context>
+</TS>
