bdd1776faa26e5a09bc404288f37fcbe90cc147b
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2021-02-04 19:04:49 +0000

    tagging qmmp-0.12.11

---
+++ tags/qmmp-0.12.11/src/plugins/Input/flac/translations/flac_plugin_cs.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="cs">
+<context>
+    <name>DecoderFLACFactory</name>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="55"/>
+        <source>FLAC Plugin</source>
+        <translation>Modul FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="57"/>
+        <source>FLAC Files</source>
+        <translation>Soubory FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="229"/>
+        <source>About FLAC Audio Plugin</source>
+        <translation>O modulu FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="230"/>
+        <source>Qmmp FLAC Audio Plugin</source>
+        <translation>Vstupní modul Qmmp FLAC</translation>
+    </message>
+    <message>
+        <location filename="../decoderflacfactory.cpp" line="231"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
