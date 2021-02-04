69409551bb58d16449a222843ddb353bd4710b4a
trialuser02 <trialuser02@90c681e8-e032-0410-971d-27865f9a5e38>
2021-02-04 19:03:39 +0000

    tagging qmmp-1.4.4

---
+++ tags/qmmp-1.4.4/src/plugins/Input/mpeg/translations/mpeg_plugin_tr.ts
@@ -0,0 +1,164 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="tr">
+<context>
+    <name>DecoderMPEGFactory</name>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="179"/>
+        <source>MPEG Plugin</source>
+        <translation>MPEG Eklentisi</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="182"/>
+        <source>MPEG Files</source>
+        <translation>MPEG Dosyaları</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="400"/>
+        <source>About MPEG Audio Plugin</source>
+        <translation>MPEG Ses Eklentisi Hakkında</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="401"/>
+        <source>MPEG 1.0/2.0/2.5 layer 1/2/3 audio decoder</source>
+        <translation>MPEG 1.0 / 2.0 / 2.5 katman 1/2/3 ses kod çözücü</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="402"/>
+        <source>Compiled against:</source>
+        <translation>Dayanarak derlendi</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="411"/>
+        <source>mpg123, API version: %1</source>
+        <translation>mpg123, API uyarlaması:% 1</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="415"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Yazan: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+    <message>
+        <location filename="../decodermpegfactory.cpp" line="416"/>
+        <source>Source code based on mq3 and madplay projects</source>
+        <translation>Kaynak kodu Mq3 ve madplay projeleri temellidir.</translation>
+    </message>
+</context>
+<context>
+    <name>MPEGMetaDataModel</name>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="65"/>
+        <location filename="../mpegmetadatamodel.cpp" line="68"/>
+        <location filename="../mpegmetadatamodel.cpp" line="71"/>
+        <location filename="../mpegmetadatamodel.cpp" line="74"/>
+        <source>Mode</source>
+        <translation>Kip</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="77"/>
+        <source>Protection</source>
+        <translation>Korunum</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="78"/>
+        <source>Copyright</source>
+        <translation>Telif hakkı</translation>
+    </message>
+    <message>
+        <location filename="../mpegmetadatamodel.cpp" line="79"/>
+        <source>Original</source>
+        <translation>Menşe</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../settingsdialog.ui" line="14"/>
+        <source>MPEG Plugin Settings</source>
+        <translation>MPEG Eklenti Ayarları</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="35"/>
+        <source>Decoder</source>
+        <translation>Kodlayıcı</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="41"/>
+        <source>MAD</source>
+        <translation>MAD</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="48"/>
+        <source>MPG123</source>
+        <translation>MPG123</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="77"/>
+        <source>Tag Priority</source>
+        <translation>Etiket Önceliği</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="89"/>
+        <source>First:</source>
+        <translation>İlk:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="109"/>
+        <location filename="../settingsdialog.ui" line="158"/>
+        <location filename="../settingsdialog.ui" line="207"/>
+        <source>ID3v1</source>
+        <translation>ID3v1</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="114"/>
+        <location filename="../settingsdialog.ui" line="163"/>
+        <location filename="../settingsdialog.ui" line="212"/>
+        <source>ID3v2</source>
+        <translation>ID3v2</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="119"/>
+        <location filename="../settingsdialog.ui" line="168"/>
+        <location filename="../settingsdialog.ui" line="217"/>
+        <source>APE</source>
+        <translation>APE</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="124"/>
+        <location filename="../settingsdialog.ui" line="173"/>
+        <location filename="../settingsdialog.ui" line="222"/>
+        <source>Disabled</source>
+        <translation>Etkisiz</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="138"/>
+        <source>Second:</source>
+        <translation>İkinci:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="187"/>
+        <source>Third:</source>
+        <translation>Üçüncü:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="243"/>
+        <source>Merge selected tag types</source>
+        <translation>Seçili etiket türlerini birleştir</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="262"/>
+        <source>Encodings</source>
+        <translation>Kodlamalar</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="274"/>
+        <source>ID3v1 encoding:</source>
+        <translation>ID3v1 kodlaması:</translation>
+    </message>
+    <message>
+        <location filename="../settingsdialog.ui" line="300"/>
+        <source>ID3v2 encoding:</source>
+        <translation>ID3v2 kodlaması:</translation>
+    </message>
+</context>
+</TS>
---
+++ tags/qmmp-1.4.4/src/plugins/Input/wavpack/translations/wavpack_plugin_fi.ts
@@ -0,0 +1,50 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fi">
+<context>
+    <name>DecoderWavPackFactory</name>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="38"/>
+        <source>WavPack Plugin</source>
+        <translation>WavPack-liitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="40"/>
+        <source>WavPack Files</source>
+        <translation>WavPack-tiedostot</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="176"/>
+        <source>About WavPack Audio Plugin</source>
+        <translation>Tietoja - WavPack-ääniliitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="177"/>
+        <source>Qmmp WavPack Audio Plugin</source>
+        <translation>Qmmp:n WavPack-ääniliitännäinen</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="178"/>
+        <source>WavPack library version:</source>
+        <translation>WavPack-kirjaston versio:</translation>
+    </message>
+    <message>
+        <location filename="../decoderwavpackfactory.cpp" line="180"/>
+        <source>Written by: Ilya Kotov &lt;forkotov02@ya.ru&gt;</source>
+        <translation>Toteuttanut: Ilya Kotov &lt;forkotov02@ya.ru&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>WavPackMetaDataModel</name>
+    <message>
+        <location filename="../wavpackmetadatamodel.cpp" line="70"/>
+        <source>Ratio</source>
+        <translation>Suhde</translation>
+    </message>
+    <message>
+        <location filename="../wavpackmetadatamodel.cpp" line="71"/>
+        <source>Version</source>
+        <translation>Versio</translation>
+    </message>
+</context>
+</TS>
